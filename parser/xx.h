8050152639565a0d350851654b3d7b2330916977
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2013-10-23 00:09:59 -0500

    Fixing old license

--- parser/xx.h
+++ parser/xx.h
@@ -1,19 +1,19 @@
 
 /*
- +----------------------------------------------------------------------+
- | Zephir Language                                                      |
- +----------------------------------------------------------------------+
- | Copyright (c) 2013 Zephir Team                                       |
- +----------------------------------------------------------------------+
- | This source file is subject to version 1.0 of the Zephir license,    |
- | that is bundled with this package in the file LICENSE, and is        |
- | available through the world-wide-web at the following url:           |
- | http://www.zephir-lang.com/license                                   |
- |                                                                      |
- | If you did not receive a copy of the Zephir license and are unable   |
- | to obtain it through the world-wide-web, please send a note to       |
- | license@zephir-lang.com so we can mail you a copy immediately.       |
- +----------------------------------------------------------------------+
+ +--------------------------------------------------------------------------+
+ | Zephir Language                                                          |
+ +--------------------------------------------------------------------------+
+ | Copyright (c) 2013 Zephir Team and contributors                          |
+ +--------------------------------------------------------------------------+
+ | This source file is subject the MIT license, that is bundled with        |
+ | this package in the file LICENSE, and is available through the           |
+ | world-wide-web at the following url:                                     |
+ | http://zephir-lang.com/license.html                                      |
+ |                                                                          |
+ | If you did not receive a copy of the MIT license and are unable          |
+ | to obtain it through the world-wide-web, please send a note to           |
+ | license@zephir-lang.com so we can mail you a copy immediately.           |
+ +--------------------------------------------------------------------------+
 */
 
 typedef struct _xx_parser_token {
