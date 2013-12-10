7a4544522dca20551ce628a1b2d1d220a6349933
Eduar Carvajal <carvajaldiazeduar@gmail.com>
2013-12-09 23:19:46 -0500

    Add getNsClassOptimizer [ci skip]

---
+++ Library/Optimizers/FunctionCall/GetNsClassOptimizer.php
@@ -0,0 +1,66 @@
+<?php
+ 
+/*
+ +--------------------------------------------------------------------------+
+ | Zephir Language                                                          |
+ +--------------------------------------------------------------------------+
+ | Copyright (c) 2013 Zephir Team and contributors                          |
+ +--------------------------------------------------------------------------+
+ | This source file is subject the MIT license, that is bundled with        |
+ | this package in the file LICENSE, and is available through the           |
+ | world-wide-web at the following url:                                     |
+ | http://zephir-lang.com/license.html                                      |
+ |                                                                          |
+ | If you did not receive a copy of the MIT license and are unable          |
+ | to obtain it through the world-wide-web, please send a note to           |
+ | license@zephir-lang.com so we can mail you a copy immediately.           |
+ +--------------------------------------------------------------------------+
+*/
+ 
+/**
+ * GetNsClassOptimizer
+ *
+ * Optimizes calls to 'get_ns_class' using internal function
+ */
+class GetNsClassOptimizer
+{
+        /**
+         *
+         * @param array $expression
+         * @param Call $call
+         * @param CompilationContext $context
+         */
+        public function optimize(array $expression, Call $call, CompilationContext $context)
+        {
+                if (!isset($expression['parameters'])) {
+                        return false;
+                }
+ 
+                if (count($expression['parameters']) != 1) {
+                        throw new CompilerException("'get_class_ns' only accepts one parameter");
+                }
+ 
+                /**
+                 * Process the expected symbol to be returned
+                 */
+                $call->processExpectedReturn($context);
+ 
+                $symbolVariable = $call->getSymbolVariable();
+                if ($symbolVariable->getType() != 'variable' && $symbolVariable->getType() != 'string') {
+                        throw new CompilerException("Returned values by functions can only be assigned to variant variables", $expression);
+                }
+ 
+                if ($call->mustInitSymbolVariable()) {
+                        $symbolVariable->initVariant($context);
+                }
+ 
+                $context->headersManager->add('kernel/string');
+ 
+                $symbolVariable->setDynamicTypes('string');
+ 
+                $resolvedParams = $call->getReadOnlyResolvedParams($expression['parameters'], $context, $expression);
+                $context->codePrinter->output('zephir_get_ns_class(' . $symbolVariable->getName() . ', ' . $resolvedParams[0] . ', 0);');
+                return new CompiledExpression('variable', $symbolVariable->getRealName(), $expression);
+        }
+ 
+}
