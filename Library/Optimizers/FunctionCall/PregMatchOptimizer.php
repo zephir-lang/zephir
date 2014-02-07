58ba78fb76c443922a056424039adbccedad3011
Tamaranga <tamaranga@gmail.com>
2014-02-07 10:54:33 +0200

    PregMatchOptimizer fix

--- Library/Optimizers/FunctionCall/PregMatchOptimizer.php
+++ Library/Optimizers/FunctionCall/PregMatchOptimizer.php
@@ -54,7 +54,7 @@ class PregMatchOptimizer
 			$matchesVariable = $context->symbolTable->getVariable($expression['parameters'][2]['value']);
 			if (!$matchesVariable->isInitialized()) {
 				$matchesVariable->initVariant($context);
-				$matchesVariable->setIsInitialized(true, $compilationContext);
+				$matchesVariable->setIsInitialized(true, $context, array());
 			}
 		} else {
 			$matchesVariable = $context->symbolTable->addTemp('variable', $context);
