cf5f260d70afac0033481ab1a90ec6d2bd582d3a
Gusakov Nikita <dev@nkt.me>
2014-02-15 10:02:40 +0400

    Added tests for ternary operator [TDD]

---
+++ unit-tests/ternary.php
@@ -0,0 +1,10 @@
+<?php
+
+$t = new Test\Ternary();
+
+assert($t->testTernary1() === 101);
+assert($t->testTernary2(true) === "foo");
+assert($t->testTernary2(false) === "bar");
+assert($t->testTernaryComplex1([], "") === 101);
+assert($t->testTernaryComplex2([], "") === 106);
+assert($t->testTernaryComplex3("") === "bool");
