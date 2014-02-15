67c929dd0937cb2f56a4fbba8c1c618bb993d339
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2014-02-15 00:13:34 -0600

    Fixing install_prereqs.sh

--- unit-tests/ci/install_prereqs.sh
+++ unit-tests/ci/install_prereqs.sh
@@ -4,6 +4,6 @@ sudo apt-get -qq install re2c
 git clone https://github.com/json-c/json-c
 cd json-c
 sh autogen.sh 2&>1 /dev/null
-./configure --silent 2&>1 /dev/null
-make --silent 2&>1 /dev/null
-sudo make --silent install 2&>1 /dev/null
+./configure --silent
+make --silent
+sudo make --silent install
