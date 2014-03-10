134dc425fb6b286521ef8612b2b872323d99d424
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-10 09:13:27 +0400

    Added Phalcon\Mvc\Model\Query prototype

--- prototypes/phalcon.php
+++ prototypes/phalcon.php
@@ -1,18 +1,32 @@
 <?php
 
-namespace Phalcon;
-
-class DI
-{
-    static protected $di;
-
-    public static function setDi($di)
+namespace Phalcon {
+    class DI
     {
-        self::$di = $di;
+        static protected $di;
+
+        public static function setDi($di)
+        {
+            self::$di = $di;
+        }
+
+        public static function getDefault()
+        {
+            return self::$di;
+        }
     }
+}
 
-    public static function getDefault()
+namespace Phalcon\Mvc\Model {
+    class Query
     {
-        return self::$di;
+
+        const TYPE_SELECT = 309;
+
+        const TYPE_INSERT = 306;
+
+        const TYPE_UPDATE = 300;
+
+        const TYPE_DELETE = 303;
     }
 }
