dba1f9cff4dcaae55d5112dc27ea6f71bc0a485e
phalcon <andres@phalconphp.com>
2014-03-22 18:40:34 -0600

    Using json-c as a submodule

--- unit-tests/ci/before_install.sh
+++ unit-tests/ci/before_install.sh
@@ -4,10 +4,9 @@ phpenv rehash
 sudo apt-get -qq install re2c valgrind
 git submodule --quiet update --init --recursive
 
-git clone --depth=1 https://github.com/json-c/json-c.git
 cd json-c
-git checkout tags/json-c-0.11-20130402
 sh autogen.sh 2&>1 /dev/null
+export CLAGS="-Wall -O2"
 ./configure --silent
 make --silent
 sudo make --silent install
