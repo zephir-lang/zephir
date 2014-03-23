f8af1b6a4ea08b0787aed949c55bbb5565be4ccb
phalcon <andres@phalconphp.com>
2014-03-22 19:24:27 -0600

    Fixing zephir_create_instance_params

--- unit-tests/ci/before_install.sh
+++ unit-tests/ci/before_install.sh
@@ -3,8 +3,8 @@
 phpenv rehash
 sudo apt-get -qq install re2c valgrind
 git submodule --quiet update --init --recursive
-git checkout patch-1
 cd json-c
+git checkout patch-1
 sh autogen.sh 2&>1 /dev/null
 ./configure --silent
 make --silent
