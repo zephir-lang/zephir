bf13f6e709a32c1bc5744788541be20e8ec6a134
phalcon <andres@phalconphp.com>
2014-03-22 19:11:39 -0600

    Changing to the relevant branch

--- unit-tests/ci/before_install.sh
+++ unit-tests/ci/before_install.sh
@@ -3,7 +3,7 @@
 phpenv rehash
 sudo apt-get -qq install re2c valgrind
 git submodule --quiet update --init --recursive
-
+git checkout patch-1
 cd json-c
 sh autogen.sh 2&>1 /dev/null
 ./configure --silent
