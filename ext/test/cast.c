cffe0a9b2855e8563469262c39514a2c1ca55fb9
Дмитрий Пацура <zaets28rus@gmail.com>
2014-04-11 17:18:07 +0400

    Cast to boolean - fixes and added test on it

--- ext/test/cast.c
+++ ext/test/cast.c
@@ -12,6 +12,9 @@
 #include <Zend/zend_interfaces.h>
 
 #include "kernel/main.h"
+#include "kernel/memory.h"
+#include "kernel/operators.h"
+#include "kernel/array.h"
 
 
 /**
@@ -25,17 +28,100 @@ ZEPHIR_INIT_CLASS(Test_Cast) {
 
 }
 
-PHP_METHOD(Test_Cast, testCast1) {
+/** To int cast */
+PHP_METHOD(Test_Cast, testIntCastFromFloat) {
 
 
 	RETURN_LONG((int) 5.0);
 
 }
 
-PHP_METHOD(Test_Cast, testCast2) {
+PHP_METHOD(Test_Cast, testIntCastFromBoolean) {
 
 
 	RETURN_LONG(1);
+
+}
+
+/** To boolean cast */
+PHP_METHOD(Test_Cast, testBooleanCastFromIntTrue1) {
+
+
+	RETURN_BOOL((zend_bool) 1);
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCastFromIntTrue2) {
+
+
+	RETURN_BOOL((zend_bool) 1000);
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCastFromIntFalse) {
+
+
+	RETURN_BOOL((zend_bool) 0);
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCastFromObject) {
+
+	zval *simpleObject;
+
+	ZEPHIR_MM_GROW();
+
+	ZEPHIR_INIT_VAR(simpleObject);
+	object_init(simpleObject);
+	RETURN_MM_BOOL(zephir_get_boolval(simpleObject));
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCastFromEmptyArray) {
+
+	zval *a;
+
+	ZEPHIR_MM_GROW();
+	ZEPHIR_INIT_VAR(a);
+	array_init(a);
+
+	RETURN_MM_BOOL(zephir_get_boolval(a));
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCastFromArray) {
+
+	zval *a, *_0;
+
+	ZEPHIR_MM_GROW();
+
+	ZEPHIR_INIT_VAR(a);
+	array_init_size(a, 7);
+	ZEPHIR_INIT_VAR(_0);
+	ZVAL_LONG(_0, 1);
+	zephir_array_fast_append(a, _0);
+	ZEPHIR_INIT_BNVAR(_0);
+	ZVAL_LONG(_0, 2);
+	zephir_array_fast_append(a, _0);
+	ZEPHIR_INIT_BNVAR(_0);
+	ZVAL_LONG(_0, 3);
+	zephir_array_fast_append(a, _0);
+	ZEPHIR_INIT_BNVAR(_0);
+	ZVAL_LONG(_0, 4);
+	zephir_array_fast_append(a, _0);
+	RETURN_MM_BOOL(zephir_get_boolval(a));
+
+}
+
+PHP_METHOD(Test_Cast, testBooleanCaseFromNull) {
+
+	zval *a = NULL;
+
+	ZEPHIR_MM_GROW();
+	ZEPHIR_INIT_VAR(a);
+	ZVAL_NULL(a);
+
+	RETURN_MM_BOOL(zephir_get_boolval(a));
 
 }
 
