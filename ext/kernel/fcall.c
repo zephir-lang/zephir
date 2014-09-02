d89ea88202515846f60c9f618ba2416edefba2cf
phalcon <andres@phalconphp.com>
2014-09-02 00:16:11 -0500

    Fixing possible xdebug incompatibility

--- ext/kernel/fcall.c
+++ ext/kernel/fcall.c
@@ -390,11 +390,11 @@ int zephir_call_user_function(zval **object_pp, zend_class_entry *obj_ce, zephir
 	}
 
 	/* Xdebug fix */
-	if (fcic.function_handler && fcic.function_handler->type == ZEND_INTERNAL_FUNCTION && fcic.function_handler->op_array) {
-		fcic.function_handler->op_array.filename = "?";
-		fcic.function_handler->op_array.line_start = 0;
-		fcic.function_handler->op_array.line_end = 0;
-	}
+	//if (fcic.function_handler && fcic.function_handler->type == ZEND_INTERNAL_FUNCTION && fcic.function_handler->op_array) {
+	//	fcic.function_handler->op_array.filename = "?";
+	//	fcic.function_handler->op_array.line_start = 0;
+	//	fcic.function_handler->op_array.line_end = 0;
+	//}
 
 	/* fcic.initialized = 0; */
 	status = ZEPHIR_ZEND_CALL_FUNCTION_WRAPPER(&fci, &fcic TSRMLS_CC);
