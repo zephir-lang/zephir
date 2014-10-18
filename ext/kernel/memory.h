dde43119fbdba9ab576cdbc8625f57033d61390d
phalcon <andres@phalconphp.com>
2014-10-18 01:59:09 -0500

    Don't ptr_dtor when observed value is a reference (8)

--- ext/kernel/memory.h
+++ ext/kernel/memory.h
@@ -137,16 +137,20 @@ void ZEPHIR_FASTCALL zephir_copy_ctor(zval *destiny, zval *origin);
 /* only removes the value body of the zval */
 #define ZEPHIR_INIT_LNVAR(z)\
 	if (z) { \
-		if (Z_REFCOUNT_P(z) > 1 || Z_ISREF_P(z)) { \
+		if (Z_REFCOUNT_P(z) > 1) { \
 			Z_DELREF_P(z); \
 			ALLOC_ZVAL(z); \
 			Z_SET_REFCOUNT_P(z, 1); \
 			Z_UNSET_ISREF_P(z); \
-			ZVAL_NULL(z); \
 		} else {\
-			zephir_value_dtor(z ZEND_FILE_LINE_CC); \
-			ZVAL_NULL(z); \
+			if (!Z_ISREF_P(z)) { \
+				zephir_value_dtor(z ZEND_FILE_LINE_CC); \
+			} else { \
+				Z_SET_REFCOUNT_P(z, 1); \
+				Z_UNSET_ISREF_P(z); \
+			} \
 		} \
+		ZVAL_NULL(z); \
 	} else { \
 		zephir_memory_alloc(&z TSRMLS_CC); \
 	}
