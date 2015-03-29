0372b05ec0f318ffd1a20ab4659c9030f9077513
phalcon <andres@phalconphp.com>
2015-03-29 14:08:36 -0600

    Added improved zend_call_function for PHP 5.6

---
+++ ext/kernel/extended/fcall.h
@@ -0,0 +1,19 @@
+
+/*
+  +------------------------------------------------------------------------+
+  | Zephir Language                                                        |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2011-2015 Zephir Team (http://www.zephir-lang.com)       |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@zephir-lang.com so we can send you a copy immediately.      |
+  +------------------------------------------------------------------------+
+  | Authors: Andres Gutierrez <andres@zephir-lang.com>                     |
+  +------------------------------------------------------------------------+
+*/
+
+int zephir_call_function_opt(zend_fcall_info *fci, zend_fcall_info_cache *fci_cache TSRMLS_DC);
