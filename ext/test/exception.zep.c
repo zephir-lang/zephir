d6aa9772382f926073f7f7b5a0f989fc14e35310
Dmitry Patsura <talk@dmtry.me>
2015-04-25 23:15:48 +0900

    Fix #915 issue, fetch properties for Runtime Class Definition

--- ext/test/exception.zep.c
+++ ext/test/exception.zep.c
@@ -12,6 +12,11 @@
 #include <Zend/zend_interfaces.h>
 
 #include "kernel/main.h"
+#include "kernel/object.h"
+#include "ext/spl/spl_exceptions.h"
+#include "kernel/exception.h"
+#include "kernel/operators.h"
+#include "kernel/memory.h"
 
 
 /**
@@ -19,9 +24,35 @@
  */
 ZEPHIR_INIT_CLASS(Test_Exception) {
 
-	ZEPHIR_REGISTER_CLASS_EX(Test, Exception, test, exception, zend_exception_get_default(TSRMLS_C), NULL, 0);
+	ZEPHIR_REGISTER_CLASS_EX(Test, Exception, test, exception, zend_exception_get_default(TSRMLS_C), test_exception_method_entry, 0);
 
 	return SUCCESS;
+
+}
+
+PHP_METHOD(Test_Exception, testRuntimePropertyFetch) {
+
+	zval *message_param = NULL;
+	zval *message = NULL;
+
+	ZEPHIR_MM_GROW();
+	zephir_fetch_params(1, 1, 0, &message_param);
+
+	if (unlikely(Z_TYPE_P(message_param) != IS_STRING && Z_TYPE_P(message_param) != IS_NULL)) {
+		zephir_throw_exception_string(spl_ce_InvalidArgumentException, SL("Parameter 'message' must be a string") TSRMLS_CC);
+		RETURN_MM_NULL();
+	}
+
+	if (likely(Z_TYPE_P(message_param) == IS_STRING)) {
+		zephir_get_strval(message, message_param);
+	} else {
+		ZEPHIR_INIT_VAR(message);
+		ZVAL_EMPTY_STRING(message);
+	}
+
+
+	zephir_update_property_this(this_ptr, SL("message"), message TSRMLS_CC);
+	ZEPHIR_MM_RESTORE();
 
 }
 
