67115ba4116b9a384e46fa3eb009d692643ed4f6
steffen <steffenb198@aol.com>
2015-06-02 16:02:35 +0200

    Call parent constructors if zephir is the origin of the childs constructor
    This should restore integrity in comparison with PHP behavior.
    
    This calls the parent constructor explicitly,
    if the user hasn't defined any (child) constructor.
    
    This doesn't call the parent constructor,
    if the user has defined a (child) constructor,
    even if the child constructor doesn't do anything
    (=empty statements).

--- Library/ClassProperty.php
+++ Library/ClassProperty.php
@@ -248,7 +248,7 @@ class ClassProperty
 
             case 'array':
             case 'empty-array':
-                
+                $classDefinition = $this->classDefinition;
                 if ($this->isStatic()) {
                     $methodName = 'zephir_init_static_properties';
                     $visibility = array('internal');
@@ -257,26 +257,27 @@ class ClassProperty
                     $visibility = array('internal');
                     
                     /* Make sure a constructor exists, so that properties are initialized */
-                    $method = $compilationContext->classDefinition->getMethod('__construct');
-                    if (!$method || $method->getClassDefinition() != $this->classDefinition) {
-                        $this->classDefinition->addMethod(new ClassMethod(
-                            $compilationContext->classDefinition,
+                    $method = $classDefinition->getMethod('__construct');
+                    if (!$method || $method->getClassDefinition() != $classDefinition) {
+                        $classDefinition->setIsGeneratedConstructor(true);
+                        $classDefinition->getEventsManager()->dispatch('setMethod', array(new ClassMethod(
+                            $classDefinition,
                             array('public'),
                             '__construct',
                             null,
                             null
-                        ), null);
+                        ), null));
                     }
                 }
 
                 $constructParentMethod = null;
-                $constructMethod = $compilationContext->classDefinition->getMethod($methodName);
+                $constructMethod = $classDefinition->getMethod($methodName);
                 /**
                  * Make sure we do not steal the construct method of the parent,
                  * but initialize our own with the inherited statements to ensure
                  * valid property initialization
                  */
-                if ($constructMethod && $constructMethod->getClassDefinition() != $this->classDefinition) {
+                if ($constructMethod && $constructMethod->getClassDefinition() != $classDefinition) {
                     $constructParentMethod = $constructMethod;
                     $constructMethod = null;
                 }
@@ -308,12 +309,12 @@ class ClassProperty
 
                             $statementsBlock->setStatements($newStatements);
                             $constructMethod->setStatementsBlock($statementsBlock);
-                            $compilationContext->classDefinition->getEventsManager()->dispatch('setMethod', array($constructMethod));
+                            $classDefinition->getEventsManager()->dispatch('setMethod', array($constructMethod));
                         }
                     } else {
                         $statementsBlockBuilder = new StatementsBlockBuilder(array($this->getLetStatement()), false);
                         $constructMethod->setStatementsBlock(new StatementsBlock($statementsBlockBuilder->get()));
-                        $compilationContext->classDefinition->getEventsManager()->dispatch('setMethod', array($constructMethod));
+                        $classDefinition->getEventsManager()->dispatch('setMethod', array($constructMethod));
                     }
                 } else {
                     $statements = array();
@@ -323,8 +324,8 @@ class ClassProperty
                     $statements[] = $this->getLetStatement()->get();
                     $statementsBlock = new StatementsBlock($statements);
 
-                    $compilationContext->classDefinition->getEventsManager()->dispatch('setMethod', array(new ClassMethod(
-                        $compilationContext->classDefinition,
+                    $classDefinition->getEventsManager()->dispatch('setMethod', array(new ClassMethod(
+                        $classDefinition,
                         $visibility,
                         $methodName,
                         null,
