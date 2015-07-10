94a94c4c408fd68153644dc942987316cc347bf8
Steffen <steffenb198@aol.com>
2015-07-10 10:30:09 +0200

    Migrate to new container based travis infrastructure

--- unit-tests/ci/before_install.sh
+++ unit-tests/ci/before_install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 phpenv rehash
-sudo apt-get update
-sudo apt-get -qq install re2c valgrind
 git submodule --quiet update --init --recursive
 cd json-c
 git checkout patch-1
 sh autogen.sh 2&>1 /dev/null
-./configure --silent
+./configure --silent --prefix=$TRAVIS_BUILD_DIR/build
 make --silent
-sudo make --silent install
+make --silent install
