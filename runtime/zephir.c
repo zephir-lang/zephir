607c393834759e764bf7d3da07160b0e27aae722
Joey <joey.bartlett@gmail.com>
2015-10-02 17:34:05 +0100

    Use correct pointer for end_memory

--- runtime/zephir.c
+++ runtime/zephir.c
@@ -98,7 +98,7 @@ static void zephir_initialize_memory(zend_zephir_globals *zephir_globals_ptr TSR
 	}
 
 	zephir_globals_ptr->start_memory = start;
-	zephir_globals_ptr->end_memory   = start + ZEPHIR_NUM_PREALLOCATED_FRAMES;
+	zephir_globals_ptr->end_memory   = &start[ZEPHIR_NUM_PREALLOCATED_FRAMES - 1];
 
 	zephir_globals_ptr->fcache = pemalloc(sizeof(HashTable), 1);
 	zend_hash_init(zephir_globals_ptr->fcache, 128, NULL, NULL, 1); // zephir_fcall_cache_dtor
