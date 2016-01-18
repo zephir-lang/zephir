8003ec6d9f1f9f3adeccbb8d957d8be42223f367
Serghei Iakovlev <sadhooklay@gmail.com>
2016-01-19 00:14:14 +0200

    Amended phpdoc

--- Library/Backends/ZendEngine2/FcallManager.php
+++ Library/Backends/ZendEngine2/FcallManager.php
@@ -38,7 +38,11 @@ class FcallManager
 
     /**
      * Resolve internal fcall attributes to a suitable macro and ensure that it's generated during compilation
-     * @param doReturn tri-state: 0 -> no return value, 1 -> do return, 2 -> do return to given variable
+     *
+     * @param $static
+     * @param int $doReturn tri-state: 0 -> no return value, 1 -> do return, 2 -> do return to given variable
+     * @param $paramCount
+     * @return string
      */
     public function getMacro($static, $doReturn, $paramCount)
     {
