c47681aeb820597512769e5adf9ab92ee1b4e92a
dangcheng <dangcheng@hotmail.com>
2016-03-03 23:27:19 +0800

    Add "MongoDB\Driver\manage" for mongodb
    Add "MongoDB\Driver\manage" for mongodb.
    see: http://php.net/manual/en/set.mongodb.php
    http://pecl.php.net/package/mongodb
    https://github.com/mongodb/mongo-php-driver

---
+++ prototypes/manage.php
@@ -0,0 +1,35 @@
+<?php
+namespace MongoDB\Driver;
+
+class Manage
+{
+    public function __construct($uri,  $options, $driverOptions)
+    {
+
+    }
+
+    public function executeQuery($namespace, $query, $readPreference)
+    {
+
+    }
+
+    public function executeBulkWrite($namespace, $bulk, $writeConcern)
+    {
+
+    }
+
+    public function executeCommand($db, $command, $readPreference)
+    {
+
+    }
+
+    public function getServers()
+    {
+
+    }
+
+    public function selectServer($readPreference)
+    {
+
+    }
+}
