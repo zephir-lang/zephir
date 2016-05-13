c03647adddadd2700d8dc181dc10ea6a9f90a2d9
Herve Seignole <herve.seignole@groupepvcp.com>
2016-05-13 02:09:27 +0200

    add test on ConfigException.

---
+++ unit-tests/Zephir/Test/ConfigExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------------+
+ | Zephir Language                                                          |
+ +--------------------------------------------------------------------------+
+ | Copyright (c) 2013-2015 Zephir Team and contributors                     |
+ +--------------------------------------------------------------------------+
+ | This source file is subject the MIT license, that is bundled with        |
+ | this package in the file LICENSE, and is available through the           |
+ | world-wide-web at the following url:                                     |
+ | http://zephir-lang.com/license.html                                      |
+ |                                                                          |
+ | If you did not receive a copy of the MIT license and are unable          |
+ | to obtain it through the world-wide-web, please send a note to           |
+ | license@zephir-lang.com so we can mail you a copy immediately.           |
+ +--------------------------------------------------------------------------+
+*/
+
+namespace Zephir\Test;
+
+use Zephir\ConfigException;
+
+class ConfigExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test __contruct method.
+     */
+    public function testConstruct()
+    {
+        $configException = new ConfigException('foo', 25);
+        $this->assertSame($configException->getCode(), 25);
+    }
+}
