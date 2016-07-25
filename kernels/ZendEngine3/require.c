c5a270231d0305388e96bc5f7c37901b6fec6b4e
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2016-07-24 21:47:15 -0500

    Assigning current scope to included files

--- kernels/ZendEngine3/require.c
+++ kernels/ZendEngine3/require.c
@@ -34,7 +34,6 @@
 #define ENFORCE_SAFE_MODE    0
 #endif
 
-
 /**
  * Do an internal require to a plain php file taking care of the value returned by the file
  */
@@ -55,18 +54,20 @@ int zephir_require_ret(zval *return_value_ptr, const char *require_path)
 	}
 #endif
 
-	if (!require_path) {
-		/* @TODO, throw an exception here */
+	/* if (!memcmp(require_path, "", 0)) {
+		@TODO, throw an exception here
 		return FAILURE;
-	}
+	} */
 
 	file_handle.filename = require_path;
 	file_handle.free_filename = 0;
 	file_handle.type = ZEND_HANDLE_FILENAME;
 	file_handle.opened_path = NULL;
 	file_handle.handle.fp = NULL;
+
 	new_op_array = zend_compile_file(&file_handle, ZEND_REQUIRE);
 	if (new_op_array) {
+
 		if (file_handle.handle.stream.handle) {
 			ZVAL_NULL(&dummy);
 			if (!file_handle.opened_path) {
@@ -76,25 +77,28 @@ int zephir_require_ret(zval *return_value_ptr, const char *require_path)
 			zend_hash_add(&EG(included_files), file_handle.opened_path, &dummy);
 			zend_destroy_file_handle(&file_handle);
 		}
+
+		new_op_array->scope = EG(scope);
 		zend_execute(new_op_array, &local_retval);
+
 		if (return_value_ptr) {
 			zval_ptr_dtor(return_value_ptr);
 			ZVAL_COPY_VALUE(return_value_ptr, &local_retval);
 		} else {
 			zval_ptr_dtor(&local_retval);
 		}
+
 		destroy_op_array(new_op_array);
 		efree_size(new_op_array, sizeof(zend_op_array));
 
 		if (EG(exception)) {
 			ret = FAILURE;
-		}
-		else {
+		} else {
 			ret = SUCCESS;
 		}
+
 		return ret;
-	}
-	else {
+	} else {
 		zend_destroy_file_handle(&file_handle);
 	}
 
