58d81e22e88a8959a8ea0bf0ec32df2958a05116
Hervé Seignole <herve.seignole@gmail.com>
2016-08-09 21:09:20 +0200

    Fix risky unit tests (#1266)
    * Fix warning when the file doesn't exists.
    
    * fix class name TEst will be Test.
    
    * Fix Risky tests with PHP Unit 5.3 and +.
    
    * Add filter and whitelist section for coverage.
    
    * Add the possibility to use phpdbg to launch the unit tests.
    
    * add test on ConfigException.
    
    * add some tests on Config class.
    
    * add tests on Utils class.
    
    * fix whitespace found at the end of line.
    
    * fix unit test for PHP < 7, var_export of float(1) = 1.
    
    * Delete of PHP 5.3.X and < code.
    
    * Add an init of variable hasChanged to fix a warning.
    
    * Finally in Windows, it's too \n, strange.

--- unit-tests/Extension/VarsTest.php
+++ unit-tests/Extension/VarsTest.php
@@ -21,87 +21,166 @@ namespace Extension;
 
 class VarsTest extends \PHPUnit_Framework_TestCase
 {
-    public function testVarDump()
+    /**
+     * Test var_dump function (array and string).
+     */
+    public function testVarDumpArrayAndString()
     {
-        ob_start();
+        $this->setOutputCallback(
+            function ($output) {
+                /* To prevent differences between PHP version */
+                return str_replace(array("\n", ' '), '', $output);
+            }
+        );
+
+        $this->expectOutputString(
+            'array(3){' .
+            '[0]=>int(1)' .
+            '[1]=>string(5)"world"' .
+            '[2]=>bool(false)}' .
+            'string(5)"hello"'
+        );
 
         $t = new \Test\Vars();
-        $t->testVarDump();
-        $t->testVarDump2param(3.1, true);
-        $t->testVarDump3param(3.1, true, array(1, 2, 3));
-        $t->testVarDump2param(3.1, true);
 
-        ob_clean();
+        $t->testVarDump();
+    }
+
+    /**
+     * Test var_dump function with two params.
+     */
+    public function testVarDumpWithTwoParams()
+    {
+        $this->expectOutputString(
+            'float(3.1)' . "\n" .
+            'bool(true)' . "\n"
+        );
+
+        $t = new \Test\Vars();
+
+        $t->testVarDump2param(3.1, true);
+    }
+
+    /**
+     * Test var_dump function with three params.
+     */
+    public function testVarDumpWithThreeParams()
+    {
+        $this->setOutputCallback(
+            function ($output) {
+                return str_replace(array("\n", ' '), '', $output);
+            }
+        );
+
+        $this->expectOutputString(
+            'float(3.1)' .
+            'bool(true)' .
+            'array(3){[0]=>int(1)[1]=>int(2)[2]=>int(3)}'
+        );
+
+        $t = new \Test\Vars();
+
+        $t->testVarDump3param(3.1, true, array(1, 2, 3));
     }
 
     public function testVarExport()
     {
-        ob_start();
+        $this->setOutputCallback(
+            function ($output) {
+                return str_replace(array("\n", ' '), '', $output);
+            }
+        );
+
+        $this->expectOutputString(
+            "array(0=>1,1=>'world',2=>false,)'hello'"
+        );
 
         $t = new \Test\Vars();
         $this->assertEquals($t->testVarExport(), "'hello'");
-
-        ob_clean();
     }
 
     public function test88Issue()
     {
-        ob_start();
-
         $t = new \Test\Vars();
-        $t->test88Issue('foo', 'bar');
-        $t->test88IssueParam2InitString('foo', 'bar');
 
-        ob_clean();
+        $this->expectOutputString(
+            'string(3) "foo"' . "\n" .
+            'string(3) "bar"' . "\n" .
+            "'foo'" .
+            "'bar'" .
+            "'bar'"
+        );
+
+        $t->test88Issue('foo', 'bar');
+
+        $t->test88IssueParam2InitString('foo', 'bar');
     }
 
     public function testVarDumpAndCountOptimizer()
     {
-        ob_start();
+        $this->expectOutputString(
+            'int(5)' . "\n" .
+            '5'
+        );
 
         $t = new \Test\Vars();
         $t->testCountOptimizerVarDumpAndExport(array(1, 2, 3, 4, 5));
-
-        ob_clean();
     }
 
     public function testArrayTypeVarDumpAndExport()
     {
-        ob_start();
+        $this->setOutputCallback(
+            function ($output) {
+                return str_replace(array("\n", ' '), '', $output);
+            }
+        );
+
+        $this->expectOutputString(
+            'array(3){[0]=>int(1)[1]=>int(2)[2]=>int(3)}' .
+            'array(0=>1,1=>2,2=>3,)'
+        );
 
         $t = new \Test\Vars();
         $t->testArrayTypeVarDumpAndExport(array(1, 2, 3));
-
-        ob_clean();
     }
 
     public function testIntVarDump()
     {
-        ob_start();
+        $this->expectOutputString(
+            'int(1)' . "\n" .
+            '1'
+        );
 
         $t = new \Test\Vars();
         $t->testIntVarDump();
-
-        ob_clean();
     }
 
     public function testDoubleVarDump()
     {
-        ob_start();
+        if (PHP_VERSION_ID < 70000) {
+            $this->expectOutputString(
+                'float(1)' . "\n" .
+                '1'
+            );
+        } else {
+            $this->expectOutputString(
+                'float(1)' . "\n" .
+                '1.0'
+            );
+        }
 
         $t = new \Test\Vars();
         $t->testDoubleVarDump();
-
-        ob_clean();
     }
 
     public function testBoolVarDump()
     {
-        ob_start();
+        $this->expectOutputString(
+            'bool(true)' . "\n" .
+            'true'
+        );
 
         $t = new \Test\Vars();
         $t->testBoolVarDump();
-
-        ob_clean();
     }
 }
