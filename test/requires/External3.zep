313d0cdc11389e43dd038418406e213005c9243d
Wojciech Ślawski <jurigag@gmail.com>
2017-03-26 13:55:13 +0200

    Some test for require

---
+++ test/requires/External3.zep
@@ -0,0 +1,18 @@
+
+/**
+ * External3 operations
+ */
+
+namespace Test\Requires;
+
+class External3
+{
+	protected someVariable;
+
+	public function req(var path, var requires)
+	{
+		ob_clean();
+		require path;
+		requires->setContent(ob_get_contents());
+	}
+}
--- unit-tests/Extension/RequiresTest.php
+++ unit-tests/Extension/RequiresTest.php
@@ -19,12 +19,21 @@
 
 namespace Extension;
 
+use Test\Requires;
+
 class RequiresTest extends \PHPUnit_Framework_TestCase
 {
     public function testRequireExternal1()
     {
-        $r = new \Test\Requires();
+        $r = new Requires();
         $this->assertSame($r->requireExternal1(__DIR__ . '/php/require-me-1.php'), array(1, 2, 3));
         $this->assertTrue($r->requireExternal1(__DIR__ . '/php/require-me-2.php') && defined('REQUIRE_ME'));
+    }
+
+    public function testRequireExternal3()
+    {
+        $r = new Requires();
+        $output = $r->requireExternal3(__DIR__ . '/php/require-me-3.php');
+        $this->assertSame("test", $output);
     }
 }
