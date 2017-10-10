35223068ec773452a044309af3427058af3df0dd
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-10 10:09:16 +0300

    Updated Copyright

--- Library/Bootstrap.php
+++ Library/Bootstrap.php
@@ -1,15 +1,14 @@
 <?php
 
 /*
- +------------------------------------------------------------------------+
- | Zephir                                                                 |
- | Copyright (c) 2013-present Zephir (https://zephir-lang.com/)           |
- |                                                                        |
- | This source file is subject the MIT license, that is bundled with      |
- | this package in the file LICENSE, and is available through the         |
- | world-wide-web at the following url:                                   |
- | http://zephir-lang.com/license.html                                    |
- +------------------------------------------------------------------------+
+ +--------------------------------------------------------------------------+
+ | Zephir                                                                   |
+ | Copyright (c) 2013-present Zephir (https://zephir-lang.com/)             |
+ |                                                                          |
+ | This source file is subject the MIT license, that is bundled with this   |
+ | package in the file LICENSE, and is available through the world-wide-web |
+ | at the following url: http://zephir-lang.com/license.html                |
+ +--------------------------------------------------------------------------+
  */
 
 namespace Zephir;
