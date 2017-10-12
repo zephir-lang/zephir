021154c79f90b66a877af796f72cc4075d02d2ed
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-12 12:42:30 +0300

    Update copyright notice

--- Library/Logger.php
+++ Library/Logger.php
@@ -3,7 +3,7 @@
 /*
  +--------------------------------------------------------------------------+
  | Zephir                                                                   |
- | Copyright (c) 2013-present Zephir (https://zephir-lang.com/)             |
+ | Copyright (c) 2013-present Zephir Team (https://zephir-lang.com/)        |
  |                                                                          |
  | This source file is subject the MIT license, that is bundled with this   |
  | package in the file LICENSE, and is available through the world-wide-web |
