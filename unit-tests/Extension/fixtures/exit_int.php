35046d774a4687e7642091f1f3d27f48eb1ec431
Daniel Hunsaker <danhunsaker@gmail.com>
2018-06-02 23:19:53 -0600

    Fix some testing settings
    Make tests more portable, so they can be run on users' systems as easily as on CI services.
    
    - Load `test` module dynamically if it isn't globally installed.
    - Use `composer.json` to indicate that PHPUnit 5.7 is acceptable when 4.8 isn't required, and allow Composer to determine which is appropriate.
    - Fix `Issue1404Test.php` to use a regex that allows the version string to include distro data, and silently ignore it. (And also the relevant optimizer, while we're here.)
    
    Also improve the way Travis tests are run:
    
    - Run tests without Valgrind to get detailed output and code coverage without risking timeouts, then with Valgrind but no code coverage or detailed output to get memory leak information as well.

--- unit-tests/Extension/fixtures/exit_int.php
+++ unit-tests/Extension/fixtures/exit_int.php
@@ -1,5 +1,9 @@
 <?php
 // unit-tests/Extension/fixtures/exit_int.php
+if (!extension_loaded('test') && ini_get('enable_dl') == '1') {
+    $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+    dl($prefix . 'test.' . PHP_SHLIB_SUFFIX);
+}
 $argv = $_SERVER['argv'];
 if (isset($argv[1])) {
     $t = new \Test\ExitDie();
