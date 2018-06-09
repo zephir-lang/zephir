1a47a494ae373130903f3483f9844b9889a2c4f2
Daniel Hunsaker <danhunsaker@gmail.com>
2018-06-09 12:28:29 -0600

    Allow extension to be loaded prior to the tests
    If `extension=test` (or `extension=php_test.dll`) is in the `php.ini`, or otherwise loaded prior to the tests themselves, the tests should be satisfied with that and continue on running. Unfortunately, I flubbed the logic there, and made it so the extension _had_ to be loaded by the tests themselves, or not at all.
    
    So, uh, let's fix that!

--- unit-tests/Extension/fixtures/exit.php
+++ unit-tests/Extension/fixtures/exit.php
@@ -1,8 +1,12 @@
 <?php
 // unit-tests/Extension/fixtures/exit.php
-if (!extension_loaded('test') && ini_get('enable_dl') == '1') {
-    $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
-    dl($prefix . 'test.' . PHP_SHLIB_SUFFIX);
+if (!extension_loaded('test')) {
+    if (ini_get('enable_dl') == '1') {
+        $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+        dl($prefix . 'test.' . PHP_SHLIB_SUFFIX);
+    } else {
+        exit('"test" extension not loaded; cannot run tests without it');
+    }
 }
 $t = new \Test\ExitDie();
 $t->testExit();
