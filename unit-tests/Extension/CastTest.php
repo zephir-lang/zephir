b05b160b70ed217ece64cd2d0135614b68880b31
AlexNDRmac <alexndr.mac@gmail.com>
2018-06-24 18:07:50 +0300

    fixed Cast Resource to Integer

--- unit-tests/Extension/CastTest.php
+++ unit-tests/Extension/CastTest.php
@@ -17,91 +17,113 @@ use Test\Cast;
 
 class CastTest extends \PHPUnit_Framework_TestCase
 {
+    private $test;
+
+    public function setUp()
+    {
+        $this->test = new Cast();
+    }
+
+    public function tearDown()
+    {
+        $this->test = null;
+    }
+
     public function testIntCast()
     {
-        $t = new Cast();
-
         /**
          * Value
          */
 
-        $this->assertSame(5, $t->testIntCastFromFloat());
-        $this->assertSame(1, $t->testIntCastFromBooleanTrue());
-        $this->assertSame(0, $t->testIntCastFromBooleanFalse());
-        $this->assertSame(0, $t->testIntCastFromNull());
-        $this->assertSame(0, $t->testIntCastFromStringValue());
-        $this->assertSame(0, $t->testIntCastFromEmptyArray());
-        $this->assertSame(1, $t->testIntCastFromArray());
-        $this->assertSame(1, $t->testIntCastFromStdClass());
+        $this->assertSame(5, $this->test->testIntCastFromFloat());
+        $this->assertSame(1, $this->test->testIntCastFromBooleanTrue());
+        $this->assertSame(0, $this->test->testIntCastFromBooleanFalse());
+        $this->assertSame(0, $this->test->testIntCastFromNull());
+        $this->assertSame(0, $this->test->testIntCastFromStringValue());
+        $this->assertSame(0, $this->test->testIntCastFromEmptyArray());
+        $this->assertSame(1, $this->test->testIntCastFromArray());
+        $this->assertSame(1, $this->test->testIntCastFromStdClass());
 
 
         /**
          * Variable types
          */
 
-        $this->assertSame(5, $t->testIntCastFromVariableFloat());
-        $this->assertSame(1, $t->testIntCastFromVariableBooleanTrue());
-        $this->assertSame(0, $t->testIntCastFromVariableBooleanFalse());
-        $this->assertSame(0, $t->testIntCastFromVariableNull());
+        $this->assertSame(5, $this->test->testIntCastFromVariableFloat());
+        $this->assertSame(1, $this->test->testIntCastFromVariableBooleanTrue());
+        $this->assertSame(0, $this->test->testIntCastFromVariableBooleanFalse());
+        $this->assertSame(0, $this->test->testIntCastFromVariableNull());
 
-        $this->assertSame(0, $t->testIntCastFromVariableString());
-        $this->assertSame((int) "test", $t->testIntCastFromParameterString("test"));
-        $this->assertSame((int) "1", $t->testIntCastFromParameterString("1"));
-        $this->assertSame((int) "12345", $t->testIntCastFromParameterString("12345"));
-        $this->assertSame((int) "-1", $t->testIntCastFromParameterString("-1"));
-        $this->assertSame((int) "+5", $t->testIntCastFromParameterString("+5"));
+        $this->assertSame(0, $this->test->testIntCastFromVariableString());
+        $this->assertSame((int) "test", $this->test->testIntCastFromParameterString("test"));
+        $this->assertSame((int) "1", $this->test->testIntCastFromParameterString("1"));
+        $this->assertSame((int) "12345", $this->test->testIntCastFromParameterString("12345"));
+        $this->assertSame((int) "-1", $this->test->testIntCastFromParameterString("-1"));
+        $this->assertSame((int) "+5", $this->test->testIntCastFromParameterString("+5"));
 
-        $this->assertSame(0, $t->testIntCastFromVariableEmptyArray());
-        $this->assertSame(1, $t->testIntCastFromVariableArray());
-        $this->assertSame(1, $t->testIntCastFromVariableStdClass());
+        $this->assertSame(0, $this->test->testIntCastFromVariableEmptyArray());
+        $this->assertSame(1, $this->test->testIntCastFromVariableArray());
+        $this->assertSame(1, $this->test->testIntCastFromVariableStdClass());
     }
 
     public function testFloatCast()
     {
-        $t = new Cast();
+        $this->assertSame($this->test->testFloatCastFromFloat(), 5.0);
+        $this->assertSame($this->test->testFloatCastFromBooleanTrue(), 1.0);
+        $this->assertSame($this->test->testFloatCastFromBooleanFalse(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromNull(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromEmptyArray(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromArray(), 1.0);
+        $this->assertSame($this->test->testFloatCastFromStdClass(), 1.0);
 
-        $this->assertSame($t->testFloatCastFromFloat(), 5.0);
-        $this->assertSame($t->testFloatCastFromBooleanTrue(), 1.0);
-        $this->assertSame($t->testFloatCastFromBooleanFalse(), 0.0);
-        $this->assertSame($t->testFloatCastFromNull(), 0.0);
-        $this->assertSame($t->testFloatCastFromEmptyArray(), 0.0);
-        $this->assertSame($t->testFloatCastFromArray(), 1.0);
-        $this->assertSame($t->testFloatCastFromStdClass(), 1.0);
-
-        $this->assertSame($t->testFloatCastFromVariableFloat(), 5.0);
-        $this->assertSame($t->testFloatCastFromVariableBooleanTrue(), 1.0);
-        $this->assertSame($t->testFloatCastFromVariableBooleanFalse(), 0.0);
-        $this->assertSame($t->testFloatCastFromVariableNull(), 0.0);
-        $this->assertSame($t->testFloatCastFromVariableEmptyArray(), 0.0);
-        $this->assertSame($t->testFloatCastFromVariableArray(), 1.0);
-        $this->assertSame($t->testFloatCastFromVariableStdClass(), 1.0);
+        $this->assertSame($this->test->testFloatCastFromVariableFloat(), 5.0);
+        $this->assertSame($this->test->testFloatCastFromVariableBooleanTrue(), 1.0);
+        $this->assertSame($this->test->testFloatCastFromVariableBooleanFalse(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromVariableNull(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromVariableEmptyArray(), 0.0);
+        $this->assertSame($this->test->testFloatCastFromVariableArray(), 1.0);
+        $this->assertSame($this->test->testFloatCastFromVariableStdClass(), 1.0);
     }
 
     public function testBooleanCast()
     {
-        $t = new Cast();
-
-        $this->assertTrue($t->testBooleanCastFromIntTrue1());
-        $this->assertTrue($t->testBooleanCastFromIntTrue2());
-        $this->assertFalse($t->testBooleanCastFromIntFalse());
-        $this->assertTrue($t->testBooleanCastFromObject());
-        $this->assertFalse($t->testBooleanCastFromEmptyArray());
-        $this->assertTrue($t->testBooleanCastFromArray());
-        $this->assertFalse($t->testBooleanCastFromNull());
+        $this->assertTrue($this->test->testBooleanCastFromIntTrue1());
+        $this->assertTrue($this->test->testBooleanCastFromIntTrue2());
+        $this->assertFalse($this->test->testBooleanCastFromIntFalse());
+        $this->assertTrue($this->test->testBooleanCastFromObject());
+        $this->assertFalse($this->test->testBooleanCastFromEmptyArray());
+        $this->assertTrue($this->test->testBooleanCastFromArray());
+        $this->assertFalse($this->test->testBooleanCastFromNull());
     }
 
     public function testObjectCast()
     {
-        $t = new Cast();
+        $this->assertEquals((object) 5, $this->test->testObjectCastFromInt());
+        $this->assertEquals((object) 5.0, $this->test->testObjectCastFromFloat());
+        $this->assertEquals((object) false, $this->test->testObjectCastFromFalse());
+        $this->assertEquals((object) true, $this->test->testObjectCastFromTrue());
+        $this->assertEquals((object) null, $this->test->testObjectCastFromNull());
+        $this->assertEquals((object) array(), $this->test->testObjectCastFromEmptyArray());
+        $this->assertEquals((object) array(1, 2, 3, 4), $this->test->testObjectCastFromArray());
+        $this->assertEquals((object) "", $this->test->testObjectCastFromEmptyString());
+        $this->assertEquals((object) "test string", $this->test->testObjectCastFromString());
+    }
 
-        $this->assertEquals((object) 5, $t->testObjectCastFromInt());
-        $this->assertEquals((object) 5.0, $t->testObjectCastFromFloat());
-        $this->assertEquals((object) false, $t->testObjectCastFromFalse());
-        $this->assertEquals((object) true, $t->testObjectCastFromTrue());
-        $this->assertEquals((object) null, $t->testObjectCastFromNull());
-        $this->assertEquals((object) array(), $t->testObjectCastFromEmptyArray());
-        $this->assertEquals((object) array(1, 2, 3, 4), $t->testObjectCastFromArray());
-        $this->assertEquals((object) "", $t->testObjectCastFromEmptyString());
-        $this->assertEquals((object) "test string", $t->testObjectCastFromString());
+    /**
+     * Test cases for Issue 1524
+     * Casting Resource to Integer must always return int value of Resource Id
+     *
+     * @author Alexander Andriiako <AlexNDR@phalconphp.com>
+     * @return void
+     */
+    public function testResourceCast()
+    {
+        $file = fopen(__DIR__ . '/php/exists.php', 'r');
+
+        $this->assertEquals((int) STDIN, $this->test->testCastStdinToInteger());
+        $this->assertEquals((int) STDOUT, $this->test->testCastStdoutToInteger());
+        $this->assertEquals((int) $file, $this->test->testCastFileResourceToInteger($file));
+
+        fclose($file);
     }
 }
