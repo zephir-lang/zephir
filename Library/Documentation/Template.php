196905d1d4dbd09561c2778e277dad04eb999b34
Serghei Iakovlev <serghei@phalconphp.com>
2018-10-21 14:15:19 +0300

    Updated copyright

--- Library/Documentation/Template.php
+++ Library/Documentation/Template.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Zephir.
+ *
+ * (c) Zephir Team <team@zephir-lang.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Zephir\Documentation;
 
 use Zephir\Config;
