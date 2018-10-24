7d7f39d0f2be0a197df3a245eabf512a1610b6f2
Serghei Iakovlev <serghei@phalconphp.com>
2018-10-24 08:14:20 +0300

    Test using psr extension

---
+++ unit-tests/3rdparty/oo/extend/middlewareinterfaceex.zep
@@ -0,0 +1,7 @@
+namespace Test\Oo\Extend;
+
+use Psr\Http\Server\MiddlewareInterface;
+
+interface MiddlewareInterfaceEx extends MiddlewareInterface
+{
+}
---
+++ unit-tests/ci/install-php-psr.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# This file is part of the Zephir.
+#
+# (c) Zephir Team <team@zephir-lang.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+echo -e "Install a tool for writing fast and flexible scanners in C from regular expressions"
+
+if [ "${CI}" != "true" ]; then
+	echo "This script is designed to run inside a CI container only. Stop."
+	exit 1
+fi
+
+PHP_MAJOR="$(`phpenv which php` -r 'echo phpversion();' | cut -d '.' -f 1)"
+PROJECT_ROOT=$(readlink -enq "$(dirname $0)/../../")
+
+if [ "${PHP_MAJOR}" = "5" ]; then
+	exit 0
+fi
+
+# We need this because there is no PHP 5 compatible PSR extension
+cp ${PROJECT_ROOT}/unit-tests/3rdparty/oo/extend/middlewareinterfaceex.zep ${PROJECT_ROOT}/test/oo/extend/middlewareinterfaceex.zep
+
+if [ ! -f "$HOME/.cache/php-psr/php_psr.c" ]; then
+	rm -rf "$HOME/.cache/php-psr"
+	mkdir "$HOME/.cache/php-psr"
+
+	git clone https://github.com/jbboehr/php-psr.git "$HOME/.cache/php-psr"
+fi
+
+cd "$HOME/.cache/php-psr"
+
+phpize
+./configure --with-php-config=$(phpenv which php-config) --enable-psr
+make -j"$(getconf _NPROCESSORS_ONLN)"
+
+cp "$HOME/.cache/php-psr/modules/psr.so" `$(phpenv which php-config) --extension-dir`
+
+echo extension=psr.so > "$(phpenv root)/versions/$(phpenv version-name)/etc/conf.d/psr.ini"
+php --ri psr
