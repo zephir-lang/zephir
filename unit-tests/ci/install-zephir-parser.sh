f2a8ba032cbbdac15c1894c047da194f851ab423
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-07 20:58:17 +0200

    Use latest Zephir Parser

--- unit-tests/ci/install-zephir-parser.sh
+++ unit-tests/ci/install-zephir-parser.sh
@@ -10,44 +10,19 @@
 # Exit the script if any statement returns a non-true return value
 set -e
 
-# Ensure that this is being run inside a CI container
-if [ "${CI}" != "true" ]; then
-	echo "This script is designed to run inside a CI container only. Exiting"
-	exit 1
-fi
+ZEPHIR_PARSER_VERSION=${ZEPHIR_PARSER_VERSION:-development}
 
-export PHP_MAJOR="$(`phpenv which php` -r 'echo phpversion();' | cut -d '.' -f 1)"
-export PHP_MINOR="$(`phpenv which php` -r 'echo phpversion();' | cut -d '.' -f 2)"
+pushd /tmp
 
-if [ -z ${ZEPHIR_PARSER_VERSION+x} ]; then
-	echo "The ZEPHIR_PARSER_VERSION is unset. Stop."
-	exit 1
-fi
+git clone --depth=1 -v https://github.com/phalcon/php-zephir-parser.git -b ${ZEPHIR_PARSER_VERSION}
+cd php-zephir-parser
+phpize
+./configure
+make
+make install
 
-LOCAL_SRC_DIR=${HOME}/.cache/zephir-parser/src
-LOCAL_LIB_DIR=${HOME}/.local/lib
-LOCAL_LIBRARY=${LOCAL_LIB_DIR}/zephir-parser-${ZEPHIR_PARSER_VERSION}-${PHP_MAJOR}.${PHP_MINOR}.so
+popd
 
-EXTENSION_DIR=`$(phpenv which php-config) --extension-dir`
+echo "extension=zephir_parser.so" >> $(phpenv root)/versions/$(phpenv version-name)/etc/conf.d/zephir-parser.ini
 
-if [ ! -f ${LOCAL_LIBRARY} ] || [ "${ZEPHIR_PARSER_VERSION}" = "development" ]; then
-	mkdir -p ${LOCAL_SRC_DIR}
-	mkdir -p ${LOCAL_LIB_DIR}
-
-	rm -rf ${LOCAL_SRC_DIR}/*
-	git clone --depth=1 -v https://github.com/phalcon/php-zephir-parser.git -b ${ZEPHIR_PARSER_VERSION} ${LOCAL_SRC_DIR}
-
-	bash ${LOCAL_SRC_DIR}/install
-
-	if [ ! -f "${EXTENSION_DIR}/zephir_parser.so" ]; then
-		echo "Unable to locate installed zephir_parser.so"
-		exit 1
-	fi
-
-	cp "${EXTENSION_DIR}/zephir_parser.so" ${LOCAL_LIBRARY}
-fi
-
-echo "[Zephir Parser]" > $(phpenv root)/versions/$(phpenv version-name)/etc/conf.d/zephir-parser.ini
-echo "extension=${LOCAL_LIBRARY}" >> $(phpenv root)/versions/$(phpenv version-name)/etc/conf.d/zephir-parser.ini
-
-$(phpenv which php) --ri 'Zephir Parser'
+php --ri 'Zephir Parser'
