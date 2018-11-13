0437bd071b893127f23d417606f5237e0b682761
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-13 08:14:30 +0200

    Provide windows entrypoint

---
+++ zephir.bat
@@ -0,0 +1,14 @@
+@echo off
+REM This file is part of the Zephir.
+REM
+REM (c) Zephir Team <team@zephir-lang.com>
+REM
+REM For the full copyright and license information, please view the LICENSE
+REM file that was distributed with this source code.
+cls
+
+if "%PHP_PEAR_PHP_BIN%" neq "" (
+	set PHPBIN=%PHP_PEAR_PHP_BIN%
+) else set PHPBIN=php
+
+"%PHPBIN%" "zephir" %*
