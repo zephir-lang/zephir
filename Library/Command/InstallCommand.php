1abfdc0c71a8eef4d21b785806c66971ec980588
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-15 16:33:25 +0200

    Closes #1520

--- Library/Command/InstallCommand.php
+++ Library/Command/InstallCommand.php
@@ -29,19 +29,31 @@ class InstallCommand extends ContainerAwareCommand
         $this
             ->setName('install')
             ->setDescription('Installs the extension in the extension directory (may require root password)')
-            ->addOption(
-                'dev',
-                null,
-                InputOption::VALUE_NONE,
-                'Install the extension in development mode'
-            );
+            ->addOption('dev', null, InputOption::VALUE_NONE, 'Install the extension in development mode')
+            ->setHelp($this->getInstallDevHelp());
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // TODO: Move all the stuff from the compiler
-        $this->compiler->install($input->getOption('dev'));
+        $this->compiler->install($input->getOption('dev') || PHP_DEBUG);
 
         return 0;
+    }
+
+    private function getInstallDevHelp()
+    {
+        return <<<EOT
+Installs the extension in the extension directory.
+Note: The command may require root password on Linux/Unit systems.
+
+Using "--dev" option will try installing the extension in development mode
+(debug symbols and no optimizations). An extension compiled with debugging symbols means
+you can run a program or library through a debugger and the debugger's output will be user
+friendlier. These debugging symbols also enlarge the program or library significantly.
+
+Note: Zephir development mode will be enabled silently if your PHP binary was compiled in
+a debug configuration.
+EOT;
     }
 }
