044c633af4579b8ba8ad77f3c064a06821bc5161
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-16 09:08:22 +0200

    Enable codecov verbose mode [skip appveyor]

--- .ci/after-success.sh
+++ .ci/after-success.sh
@@ -7,23 +7,25 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
+set -e +o pipefail
+
 say() {
   echo -e "$1"
 }
 
-if [ -z ${CODECOV_TOKEN+x} ]; then
-	say "The CODECOV_TOKEN variable is absent or empty.\nSkip uploading reports to Codecov"
+if [[ -z ${CODECOV_TOKEN+x} ]]; then
+	say "\nThe CODECOV_TOKEN variable is absent or empty.\nSkip uploading reports to Codecov.\n"
 	exit 0;
 fi
 
-if [ ! -z ${TRAVIS_PHP_VERSION+x} ] && [ "${TRAVIS_PHP_VERSION}" = "5.6" ]; then
-	say "Uploading reports to Codecov is disabled for PHP 5.6"
-	say "Skip"
-	exit 0;
+if [[ "${CI}" = "true" ]]; then
+	if [[ ! -z ${TRAVIS_PHP_VERSION+x} ]] && [[ "${TRAVIS_PHP_VERSION}" = "5.6" ]]; then
+		say "\nUploading reports is disabled for PHP 5.6.\nSkip uploading reports to Codecov.\n"
+		exit 0;
+	fi
 fi
 
 curl -sSl https://codecov.io/bash -o codecov.sh
 chmod +x codecov.sh
 
-./codecov.sh -h
-./codecov.sh
+./codecov.sh -v
