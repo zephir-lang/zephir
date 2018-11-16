a7e1a22ce2f9d9621e818e921f34bf83b3391f2b
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-16 08:58:11 +0200

    Correcct after-success.sh script [skip appveyor]

--- .ci/after-success.sh
+++ .ci/after-success.sh
@@ -7,13 +7,18 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
+say() {
+  echo -e "$1"
+}
+
 if [ -z ${CODECOV_TOKEN+x} ]; then
-	echo "The CODECOV_TOKEN variable is absent or empty. Skip uploading reports to Codecov"
+	say "The CODECOV_TOKEN variable is absent or empty.\nSkip uploading reports to Codecov"
 	exit 0;
 fi
 
-if [ ! -z ${TRAVIS_PHP_VERSION+x} ] && [ "${TRAVIS_PHP_VERSION}" != "5.6" ]; then
-	echo "Uploading reports to Codecov is disabled for PHP 5.6"
+if [ ! -z ${TRAVIS_PHP_VERSION+x} ] && [ "${TRAVIS_PHP_VERSION}" = "5.6" ]; then
+	say "Uploading reports to Codecov is disabled for PHP 5.6"
+	say "Skip"
 	exit 0;
 fi
 
