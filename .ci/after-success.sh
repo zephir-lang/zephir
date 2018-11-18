86580309cce18fd69115d2b4ae5693a0a97a1482
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-18 12:21:35 +0200

    Static Code Analysis for bashisms [skip appveyor]

--- .ci/after-success.sh
+++ .ci/after-success.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 #
 # This file is part of the Zephir.
 #
@@ -7,26 +7,29 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
-set -e +o pipefail
+# -e	Exit immediately if a command exits with a non-zero status.
+# -u	Treat unset variables as an error when substituting.
+set -eu
 
 PROJECT_ROOT=$(readlink -enq "$(dirname $0)/../")
-LCOV_REPORT=${PROJECT_ROOT}/unit-tests/output/lcov.info
-PHPUNIT_REPORT=${PROJECT_ROOT}/unit-tests/output/clover.xml
 
-if [[ -z ${COLLECT_COVERAGE+x} ]] || [[ "$COLLECT_COVERAGE" != "true" ]]; then
-	echo -e "\nUploading reports is not enabled.\nSkip uploading reports to Codecov.\n"
+gcov_report=${PROJECT_ROOT}/unit-tests/output/lcov.info
+phpunit_report=${PROJECT_ROOT}/unit-tests/output/clover.xml
+
+if [ -z ${COLLECT_COVERAGE+x} ] || [ "$COLLECT_COVERAGE" != "true" ]; then
+	printf "Uploading reports is not enabled.\nSkip uploading reports to Codecov.\n"
 	exit 0
 fi
 
-if [[ "${CI}" = "true" ]]; then
-	if [[ "$TRAVIS_PHP_VERSION" = "5.6" ]]; then
-		echo -e "\nUploading reports is disabled for PHP 5.6.\nSkip uploading reports to Codecov.\n"
+if [ ! -z ${TRAVIS+x} ] && [ "$TRAVIS" = "true" ]; then
+	if [ "$TRAVIS_PHP_VERSION" = "5.6" ]; then
+		printf "Collecting coverage date is disabled for PHP 5.6.\nSkip uploading reports to Codecov.\n"
 		exit 0
 	fi
 fi
 
-if [[ $(command -v lcov 2>/dev/null) = "" ]]; then
-	echo -e "lcov does not exist.\nSkip capturing coverage data."
+if [ $(which lcov 2>/dev/null) = "" ]; then
+	printf "lcov does not exist.\nSkip capturing coverage data.\n"
 else
 	# Capture coverage data
 	lcov \
@@ -36,38 +39,36 @@ else
 		--base-directory=${PROJECT_ROOT} \
 		--capture \
 		--compat-libtool \
-		--output-file ${LCOV_REPORT} 2>/dev/null
+		--output-file ${gcov_report} 2>/dev/null
 
 	# Remove files matching non-project patterns
 	lcov \
 		--quiet \
-		--remove ${LCOV_REPORT} "/usr*" \
-		--remove ${LCOV_REPORT} "${HOME}/.phpenv/*" \
+		--remove ${gcov_report} "/usr*" \
+		--remove ${gcov_report} "${HOME}/.phpenv/*" \
 		--compat-libtool \
-		--output-file ${LCOV_REPORT} 2>/dev/null
+		--output-file ${gcov_report} 2>/dev/null
 
 	# FIXME: Fix the report
 	# 	Cannot open source file ${PROJECT_ROOT}/kernel/fcall.h
-	sed -i.bak s_${PROJECT_ROOT}/kernel_${PROJECT_ROOT}/ext/kernel_g ${LCOV_REPORT}
+	sed -i.bak s_${PROJECT_ROOT}/kernel_${PROJECT_ROOT}/ext/kernel_g ${gcov_report}
 fi
 
 # Note: to upload a coverage report, set the CODECOV_TOKEN environment variable
 #    export CODECOV_TOKEN=<codecov token>
 
-if [[ -z ${CODECOV_TOKEN+x} ]]; then
-	echo -e "\nThe CODECOV_TOKEN variable is absent or empty.\nSkip uploading reports to Codecov.\n"
+if [ -z ${CODECOV_TOKEN+x} ]; then
+	printf "The CODECOV_TOKEN variable is absent or empty.\nSkip uploading reports to Codecov.\n"
 	exit 0
 fi
 
-curl -sSl https://codecov.io/bash -o codecov.sh
-chmod +x codecov.sh
+curl -sSl https://codecov.io/bash -o "${HOME}/bin/codecov"
+chmod +x "${HOME}/bin/codecov"
 
-if [[ -f "${LCOV_REPORT}" ]]; then
-	echo -e "Uploading coverage report: ${LCOV_REPORT}...\n"
-	./codecov.sh -f "${LCOV_REPORT}"
+if [ -f ${gcov_report} ]; then
+	codecov -f ${gcov_report}
 fi
 
-if [[ -f "${PHPUNIT_REPORT}" ]]; then
-	echo -e "Uploading coverage report: ${PHPUNIT_REPORT}...\n"
-	./codecov.sh -f "${PHPUNIT_REPORT}"
+if [ -f ${phpunit_report} ]; then
+	codecov -f ${phpunit_report}
 fi
