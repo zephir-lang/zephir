7247f301e84762cec6c8a7531e37e614f712cbc2
Serghei Iakovlev <serghei@phalconphp.com>
2018-11-18 21:43:06 +0200

    Fixed "create_symbol_table" for ZendEngine3
    Closes: #1621, #1488, #1403, #1428

--- ext/test/requires.zep.c
+++ ext/test/requires.zep.c
@@ -14,9 +14,11 @@
 #include "kernel/main.h"
 #include "kernel/memory.h"
 #include "kernel/require.h"
-#include "kernel/operators.h"
 #include "kernel/fcall.h"
 #include "kernel/object.h"
+#include "ext/spl/spl_exceptions.h"
+#include "kernel/exception.h"
+#include "kernel/operators.h"
 
 
 ZEPHIR_INIT_CLASS(Test_Requires) {
@@ -71,13 +73,12 @@ PHP_METHOD(Test_Requires, requireExternal2) {
 PHP_METHOD(Test_Requires, requireExternal3) {
 
 	zend_long ZEPHIR_LAST_CALL_STATUS;
-	zval *path, path_sub, external3, _0, _1$$3;
+	zval *path, path_sub, external3, _0;
 	zval *this_ptr = getThis();
 
 	ZVAL_UNDEF(&path_sub);
 	ZVAL_UNDEF(&external3);
 	ZVAL_UNDEF(&_0);
-	ZVAL_UNDEF(&_1$$3);
 
 	ZEPHIR_MM_GROW();
 	zephir_fetch_params(1, 1, 0, &path);
@@ -85,12 +86,8 @@ PHP_METHOD(Test_Requires, requireExternal3) {
 
 
 	ZEPHIR_INIT_VAR(&_0);
-	ZEPHIR_GET_CONSTANT(&_0, "PHP_MAJOR_VERSION");
-	if (ZEPHIR_IS_LONG(&_0, 5)) {
-		ZEPHIR_INIT_VAR(&_1$$3);
-		zephir_create_symbol_table(TSRMLS_C);
-		
-	}
+	zephir_create_symbol_table(TSRMLS_C);
+	
 	ZEPHIR_INIT_VAR(&external3);
 	object_init_ex(&external3, test_requires_external3_ce);
 	if (zephir_has_constructor(&external3 TSRMLS_CC)) {
@@ -115,6 +112,68 @@ PHP_METHOD(Test_Requires, setContent) {
 
 
 	zephir_update_property_zval(this_ptr, SL("content"), content);
+
+}
+
+PHP_METHOD(Test_Requires, renderTemplate) {
+
+	zend_string *_3$$3;
+	zend_ulong _2$$3;
+	zval *templatePath_param = NULL, *params, params_sub, _0, key, value, _5, *_1$$3, _4$$4;
+	zval templatePath;
+	zval *this_ptr = getThis();
+
+	ZVAL_UNDEF(&templatePath);
+	ZVAL_UNDEF(&params_sub);
+	ZVAL_UNDEF(&_0);
+	ZVAL_UNDEF(&key);
+	ZVAL_UNDEF(&value);
+	ZVAL_UNDEF(&_5);
+	ZVAL_UNDEF(&_4$$4);
+
+	ZEPHIR_MM_GROW();
+	zephir_fetch_params(1, 2, 0, &templatePath_param, &params);
+
+	if (UNEXPECTED(Z_TYPE_P(templatePath_param) != IS_STRING && Z_TYPE_P(templatePath_param) != IS_NULL)) {
+		zephir_throw_exception_string(spl_ce_InvalidArgumentException, SL("Parameter 'templatePath' must be a string") TSRMLS_CC);
+		RETURN_MM_NULL();
+	}
+	if (EXPECTED(Z_TYPE_P(templatePath_param) == IS_STRING)) {
+		zephir_get_strval(&templatePath, templatePath_param);
+	} else {
+		ZEPHIR_INIT_VAR(&templatePath);
+		ZVAL_EMPTY_STRING(&templatePath);
+	}
+
+
+	ZEPHIR_INIT_VAR(&_0);
+	zephir_create_symbol_table(TSRMLS_C);
+	
+	if (Z_TYPE_P(params) == IS_ARRAY) {
+		zephir_is_iterable(params, 0, "test/requires.zep", 47);
+		ZEND_HASH_FOREACH_KEY_VAL(Z_ARRVAL_P(params), _2$$3, _3$$3, _1$$3)
+		{
+			ZEPHIR_INIT_NVAR(&key);
+			if (_3$$3 != NULL) { 
+				ZVAL_STR_COPY(&key, _3$$3);
+			} else {
+				ZVAL_LONG(&key, _2$$3);
+			}
+			ZEPHIR_INIT_NVAR(&value);
+			ZVAL_COPY(&value, _1$$3);
+			ZEPHIR_CPY_WRT(&_4$$4, &value);
+			if (zephir_set_symbol(&key, &_4$$4 TSRMLS_CC) == FAILURE) {
+				return;
+			}
+		} ZEND_HASH_FOREACH_END();
+		ZEPHIR_INIT_NVAR(&value);
+		ZEPHIR_INIT_NVAR(&key);
+	}
+	ZEPHIR_OBSERVE_OR_NULLIFY_PPZV(&_5);
+	if (zephir_require_zval_ret(&_5, &templatePath TSRMLS_CC) == FAILURE) {
+		RETURN_MM_NULL();
+	}
+	RETURN_CCTOR(&_5);
 
 }
 
