4849d6bdf6554466eafd8797fd028e101d7b1262
Serghei Iakovlev <serghei@phalconphp.com>
2018-12-17 07:34:26 +0200

    Move Travis test to the separated file

---
+++ .ci/run-tests.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# This file is part of the Zephir.
+#
+# (c) Zephir Team <team@zephir-lang.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+# -e	Exit immediately if a command exits with a non-zero status.
+# -u	Treat unset variables as an error when substituting.
+set -eu
+
+if [ -z ${CI+x} ] || [ "$CI" != "true" ]; then
+	printf "This script is designed to run inside a CI container only.\nAborting.\n"
+	exit 1
+fi
+
+if [ "$(php-config --vernum)" -lt "70000" ]; then
+	test_suite="Extension_Php56"
+elif [ "$(php-config --vernum)" -lt "70200" ]; then
+	test_suite="Extension_Php70"
+else
+	test_suite="Extension_Php72"
+fi
+
+php \
+	-d extension=ext/modules/test.so \
+	vendor/bin/simple-phpunit \
+	--colors=always \
+	--bootstrap unit-tests/ext-bootstrap.php \
+	--testsuite ${test_suite}
+
+php \
+	vendor/bin/simple-phpunit \
+	--colors=always \
+	--testsuite Zephir
