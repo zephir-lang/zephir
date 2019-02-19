ee777ea53972f9905045838a70bd12dc25e90d51
AlexNDRmac <alexndr.mac@gmail.com>
2019-02-19 13:53:28 +0200

    Fix Config, fix naming, improve tests

--- unit-tests/Extension/ExtensionInfoTest.php
+++ unit-tests/Extension/ExtensionInfoTest.php
@@ -21,12 +21,16 @@ class ExtensionInfoTest extends TestCase
     public function directiveProvider(): array
     {
         return [
-            ['Zephir Test Extension => enabled', true],
-            ['Zephir test variable => Value', true],
-            ['zephir.superpower', true],
+            ['Test Extension => enabled', true],
+            ['Test Extension support => Value', true],
+            ['Test variable => Value', true],
+            ['extension.test_ini_variable => On => On', true],
+            ['ini-entry.my_setting_1', true],
+            ['test.db.my_setting_1', true],
             ['test.orm.cache_enable', true],
-            ['test.my_setting_1', true],
             ['test.test.my_setting_1', false],
+            ['test.test.test_setting_1', false],
+            ['test.test.', false],
         ];
     }
 
@@ -37,9 +41,11 @@ class ExtensionInfoTest extends TestCase
     public function shouldBeWithoutDuplicatesNamespace(string $var, bool $contains)
     {
         ob_start();
+
         phpinfo(INFO_MODULES);
         $phpinfo = ob_get_contents();
-        ob_end_clean() ;
+
+        ob_end_clean();
 
         if ($contains) {
             $this->assertContains($var, $phpinfo);
