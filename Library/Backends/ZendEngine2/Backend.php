9d16cacc946d756ec4dba07a7f5e1766e848fb06
Dreamszhu <dreamsxin@gmail.com>
2019-02-24 06:21:36 +0800

    Update Backend.php

--- Library/Backends/ZendEngine2/Backend.php
+++ Library/Backends/ZendEngine2/Backend.php
@@ -1183,12 +1183,12 @@ class Backend extends BaseBackend
 
     public function forStatement(Variable $exprVariable, $keyVariable, $variable, $duplicateKey, $duplicateHash, $statement, $statementBlock, CompilationContext $compilationContext)
     {
-        /**
+        /*
          * Create a hash table and hash pointer temporary variables.
          */
         $arrayPointer = $compilationContext->symbolTable->addTemp('HashPosition', $compilationContext);
         $arrayHash = $compilationContext->symbolTable->addTemp('HashTable', $compilationContext);
-        /**
+        /*
          * Create a temporary zval to fetch the items from the hash.
          */
         $compilationContext->headersManager->add('kernel/fcall');
