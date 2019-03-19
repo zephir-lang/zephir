bc53e1a4954f50c572d5e03f633720c4e3c0d37f
Dreamszhu <dreamsxin@qq.com>
2019-03-19 10:42:40 +0800

    Fix #1831

--- unit-tests/Extension/Properties/PropertyArrayTest.php
+++ unit-tests/Extension/Properties/PropertyArrayTest.php
@@ -21,4 +21,31 @@ class PropertyArrayTest extends TestCase
         $t = new PropertyArray();
         $this->assertSame([1], $t->someArray);
     }
+
+    public function testIssues1831()
+    {
+		$header = [
+			"header 1",
+			"header 2",
+			"header 3"
+		];
+
+		$data = [
+			"header 1",
+			"header 2",
+			"header 3",
+			"data 1",
+			"data 2",
+			"data 3",
+			"data 4",
+			"data 5",
+			"data 6",
+			"data 7",
+			"data 8"
+		];
+
+        $t = new PropertyArray();
+		$t->setOtherArray($data);
+        $this->assertSame($header, $t->testIssues1831());
+    }
 }
