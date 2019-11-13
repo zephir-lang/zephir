5c805cdd3def6a69b9c954d1d0777a67f079f60b
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-13 12:28:22 +0200

    Beautify Sharness test for Stubs

--- unit-tests/sharness/t0005-stubs.sh
+++ unit-tests/sharness/t0005-stubs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2034
-test_description="Test generate IDE stubs"
+test_description="Test generates IDE stubs"
 
 # shellcheck disable=SC1091
 source ./setup.sh
@@ -15,53 +15,35 @@ zephir_stubs() {
 }
 
 # This test generates Stubs once and all other tests uses this build artifacts
-test_expect_success "Should generate Stubs" "
-  zephir_stubs &&
-  test -d ./ide/0.0.1/Stubs
-"
+test_expect_success "Should generate Stubs" \
+  "zephir_stubs && test -d ./ide/0.0.1/Stubs"
 
 # See: https://github.com/phalcon/zephir/issues/1922
-test_expect_success "Should properly generate type hint" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test_cmp expected/Issue_1922.zep.php ide/0.0.1/Stubs/Issue_1922.zep.php
-"
+test_expect_success "Should properly generate type hint" \
+  "test_cmp expected/Issue_1922.zep.php ide/0.0.1/Stubs/Issue_1922.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1778
-test_expect_success "Should properly namespace imports (use block)" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test_cmp expected/Issue_1778.zep.php ide/0.0.1/Stubs/Issue_1778.zep.php
-"
+test_expect_success "Should properly namespace imports (use block)" \
+  "test_cmp expected/Issue_1778.zep.php ide/0.0.1/Stubs/Issue_1778.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1900
-test_expect_success "Should properly generate return types for stubs" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test_cmp expected/Issue_1900.zep.php ide/0.0.1/Stubs/Issue_1900.zep.php
-"
+test_expect_success "Should properly generate return types for stubs" \
+  "test_cmp expected/Issue_1900.zep.php ide/0.0.1/Stubs/Issue_1900.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1907
-test_expect_success "Should properly generate Namespace for extends" "
-  cd $FIXTURESDIR/stubs/issues &&
-  zephirc generate --no-ansi 2>&1 >/dev/null &&
-  zephirc stubs --no-ansi 2>&1 >/dev/null &&
-  test_cmp expected/Exception.zep.php ide/0.0.1/Stubs/Exception.zep.php
-"
+test_expect_success "Should properly generate Namespace for extends" \
+  "test_cmp expected/Exception.zep.php ide/0.0.1/Stubs/Exception.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1907
-test_expect_success "Should properly generate Namespace for extends (slash)" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test_cmp expected/Issue_1907.zep.php ide/0.0.1/Stubs/Issue_1907.zep.php
-"
+test_expect_success "Should properly generate Namespace for extends (slash)" \
+  "test_cmp expected/Issue_1907.zep.php ide/0.0.1/Stubs/Issue_1907.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1986
-test_expect_success "Should properly generate Aliases for use statements" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test_cmp expected/Issue_1986.zep.php ide/0.0.1/Stubs/Issue_1986.zep.php
-"
+test_expect_success "Should properly generate Aliases for use statements" \
+  "test_cmp expected/Issue_1986.zep.php ide/0.0.1/Stubs/Issue_1986.zep.php"
 
 # See: https://github.com/phalcon/zephir/issues/1896
-test_expect_success "Should generage CamelCase folders for stubs" "
-  cd $FIXTURESDIR/stubs/issues &&
-  test $(ls ./ide/0.0.1/Stubs/Events/ManagerInterface.zep.php | sed -e 's~\/~\\~g') = .\ide\0.0.1\Stubs\Events\ManagerInterface.zep.php
-"
+test_expect_success "Should generage CamelCase folders for stubs" \
+  "test $(ls ./ide/0.0.1/Stubs/Events/ManagerInterface.zep.php | sed -e 's~\/~\\~g') = .\ide\0.0.1\Stubs\Events\ManagerInterface.zep.php"
 
 test_done
