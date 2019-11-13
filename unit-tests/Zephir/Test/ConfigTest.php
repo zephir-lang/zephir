d6175833f4eb6273ebf035267d38329f677cc75b
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-11-14 00:40:41 +0200

    Refactor test for Config

--- unit-tests/Zephir/Test/ConfigTest.php
+++ unit-tests/Zephir/Test/ConfigTest.php
@@ -23,10 +23,14 @@ class ConfigTest extends TestCase
      */
     private $pwd;
 
+    /** @var Config $config */
+    private $config;
+
     public function setUp()
     {
         /* Store the current directory before to be change */
         $this->pwd = getcwd();
+        $this->config = new Config();
     }
 
     /**
@@ -38,62 +42,6 @@ class ConfigTest extends TestCase
             chdir($this->pwd);
         }
 
-        $this->cleanTmpConfigFile();
-    }
-
-    /**
-     * Test when we have a bad config.json file.
-     *
-     * @expectedException \Zephir\Exception
-     * @expectedExceptionMessage The config.json file is not valid or there is
-     * no Zephir extension initialized in this directory.
-     */
-    public function testConstructWithBadConfigFile()
-    {
-        chdir(\constant('ZEPHIRPATH').'/unit-tests/fixtures/badconfig');
-        new Config();
-    }
-
-    public function testGetWithoutNamespace()
-    {
-        $config = new Config();
-        $config->set('verbose', false);
-        $this->assertFalse($config->get('verbose'));
-    }
-
-    public function testGetWithNamespace()
-    {
-        $config = new Config();
-        $config->get('unused-variable', true, 'warnings');
-        $this->assertTrue($config->get('unused-variable', 'warnings'));
-    }
-
-    public function testSetWithoutNamespace()
-    {
-        $config = new Config();
-        $config->set('config', true);
-        $this->assertTrue($config->get('config'));
-    }
-
-    public function testSetWithNamespace()
-    {
-        $config = new Config();
-        $config->set('unused-variable', false, 'warnings');
-        $this->assertFalse($config->get('unused-variable', 'warnings'));
-    }
-
-    /**
-     * Test saveOnExit method.
-     */
-    public function testSaveOnExit()
-    {
-        chdir(sys_get_temp_dir());
-        $config = new Config();
-        $config->set('name', 'foo');
-        $config->dumpToFile();
-        $configJson = json_decode(file_get_contents('config.json'), true);
-        $this->assertInternalType('array', $configJson);
-        $this->assertSame($configJson['name'], 'foo');
         $this->cleanTmpConfigFile();
     }
 
@@ -110,13 +58,86 @@ class ConfigTest extends TestCase
         }
     }
 
-    /** @test */
-    public function directiveShowsWithoutDuplicatedNamespace()
+    /**
+     * Test when we have a bad config.json file.
+     *
+     * @test
+     * @expectedException \Zephir\Exception
+     * @expectedExceptionMessage The config.json file is not valid or there is
+     * no Zephir extension initialized in this directory.
+     */
+    public function constructWithBadConfigFile()
     {
-        $config = new Config();
-        $config->set('ext.some_key', 'some_value', 'test');
+        chdir(\constant('ZEPHIRPATH').'/unit-tests/fixtures/badconfig');
+        new Config();
+    }
 
-        $this->assertSame($config->get('ext.some_key', 'test'), 'some_value');
-        $this->assertSame($config->get('test.my_setting_1', 'test'), null);
+    /**
+     * Test data provider.
+     *
+     * [
+     *     'test name' => [
+     *          [$key, $value, $namespace], $expected,
+     *      ],
+     *      ...
+     * ]
+     *
+     * @return array
+     */
+    public function setConfigDataProvider(): array
+    {
+        return [
+            'set with namespace' => [
+                ['unused-variable', false, 'warnings'], false,
+            ],
+            'set without namespace' => [
+                ['config', true, null], true,
+            ],
+            'get with namespace' => [
+                ['unused-variable', true, 'warnings'], true,
+            ],
+            'get without namespace' => [
+                ['verbose', false, null], false,
+            ],
+            'directive don`t have duplicates with namespace' => [
+                ['ext.some_key', 'some_value', 'test'], 'some_value',
+            ],
+            'directive don`t have duplicates without namespace' => [
+                ['test.my_setting_1', 'test', null], 'test',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider setConfigDataProvider
+     */
+    public function shouldSetConfigParams(array $test, $expected)
+    {
+        list($key, $value, $namespace) = $test;
+        $this->config->set($key, $value, $namespace);
+
+        $actual = $this->config->get($key, $namespace);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /** @test */
+    public function shouldGetDefaultConfigParams()
+    {
+        $this->assertSame($this->config->get('test.my_setting_1', 'test'), null);
+    }
+
+    /** @test */
+    public function shouldSaveConfigOnExit()
+    {
+        chdir(sys_get_temp_dir());
+        $config = new Config();
+        $config->set('name', 'foo');
+        $config->dumpToFile();
+        $configJson = json_decode(file_get_contents('config.json'), true);
+        $this->assertInternalType('array', $configJson);
+        $this->assertSame($configJson['name'], 'foo');
+        $this->cleanTmpConfigFile();
     }
 }
