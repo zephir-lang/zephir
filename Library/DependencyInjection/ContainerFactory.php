053d8b4db3191bf95f5ea12cea3a4187d5c1bb10
Serghei Iakovlev <egrep@protonmail.ch>
2020-01-23 12:22:45 +0200

    Change the internal DI env to use prod mode after release

--- Library/DependencyInjection/ContainerFactory.php
+++ Library/DependencyInjection/ContainerFactory.php
@@ -13,9 +13,28 @@ namespace Zephir\DependencyInjection;
 
 final class ContainerFactory
 {
+    private $environment = '$kernel-environment$';
+    private $debug = false;
+
+    public function __construct()
+    {
+        /*
+         * Please do not concatenate the string bellow.
+         * The final version of this string is used to prepare release PHAR file.
+         * For more see box.json.dist file at the project root.
+         */
+        if ('$'.'kernel-environment'.'$' === $this->environment) {
+            $this->environment = 'dev';
+        }
+
+        if ('dev' === $this->environment) {
+            $this->debug = true;
+        }
+    }
+
     public function createWithConfigs(array $configs)
     {
-        $kernel = new ZephirKernel('dev', true, $configs);
+        $kernel = new ZephirKernel($this->environment, $this->debug, $configs);
         $kernel->boot();
 
         return $kernel->getContainer();
@@ -23,7 +42,7 @@ final class ContainerFactory
 
     public function create()
     {
-        $kernel = new ZephirKernel('dev', true);
+        $kernel = new ZephirKernel($this->environment, $this->debug);
         $kernel->boot();
 
         return $kernel->getContainer();
