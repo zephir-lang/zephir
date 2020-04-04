5854da78d536f32d1b4be01f560f49462bb18764
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-04 23:05:01 +0300

    Do not use cached version of the parser

--- .ci/install-zephir-parser.sh
+++ .ci/install-zephir-parser.sh
@@ -16,21 +16,18 @@ PHP_VERSION=$1
 
 echo "Install Zephir Parser using version: $ZEPHIR_PARSER_VERSION"
 
-if [ ! -f "$HOME/php-zephir-parser/LICENSE" ]; then
-  rm -rf "$HOME/php-zephir-parser"
-  git clone -b "$ZEPHIR_PARSER_VERSION" \
+git clone -b "$ZEPHIR_PARSER_VERSION" \
       --depth 1 \
       -q https://github.com/phalcon/php-zephir-parser \
-      "$HOME/php-zephir-parser"
-fi
+      php-zephir-parser
 
-cd "$HOME/php-zephir-parser" || exit 1
+cd php-zephir-parser || exit 1
 
 phpize
 ./configure --with-php-config=/usr/bin/php-config --enable-zephir_parser
 make -j"$(getconf _NPROCESSORS_ONLN)"
-
 sudo make install
+
 echo 'extension="zephir_parser.so"' |\
   sudo tee "/etc/php/$PHP_VERSION/cli/conf.d/zephir_parser.ini"
 
