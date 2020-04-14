c4f84d3298e66c72046de330b3ac5ff09e1dc62a
Serghei Iakovlev <egrep@protonmail.ch>
2020-04-14 21:27:43 +0300

    Amended directory locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,21 @@
 ;;; Directory Local Variables
 ;; For more information see (info "(emacs) Directory Variables")
 
-((zephir-mode . ((fill-column . 120)
-		 (indent-tabs-mode . nil)))
- (c-mode . ((fill-column . 80)
-	    (c-file-style . "k&r")
-	    (tab-width . 4)
-	    (c-basic-offset . 4)
-	    (indent-tabs-mode . t))))
+((nil
+  (indent-tabs-mode . nil))
+ (zephir-mode
+  (fill-column . 120))
+ (c-mode
+  (fill-column . 80)
+  (c-file-style . "k&r")
+  (tab-width . 4)
+  (c-basic-offset . 4)
+  (indent-tabs-mode . t)
+  (flycheck-checker . c/c++-gcc)
+  (flycheck-disabled-checkers . (c/c++-clang))
+  (flycheck-gcc-language-standard . "gnu99"))
+ (php-mode
+  (fill-column . 120)
+  (c-file-style . nil)
+  (indent-tabs-mode . nil)
+  (flycheck-disabled-checkers . (php-phpmd php-phpcs))))
