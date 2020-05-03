a36cadacb68a49f4aeabbcc798b70cf88219d106
AlexNDRmac <AlexNDR.mac@gmail.com>
2020-05-04 00:38:14 +0300

    Disable stub test for PHP 7.0

--- unit-tests/sharness/t0005-stubs.sh
+++ unit-tests/sharness/t0005-stubs.sh
@@ -14,6 +14,11 @@ zephir_stubs() {
   zephirc stubs --no-ansi 2>&1 >/dev/null
 }
 
+# Some stubs test requires PHP >= 7.1
+if [ $PHP_VERSION_ID -lt 71000 ]; then
+  test_set_prereq PHP70
+fi
+
 # This test generates Stubs once and all other tests uses this build artifacts
 test_expect_success "Should generate Stubs" \
   "zephir_stubs && test -d ./ide/0.0.1/Stubs"
@@ -50,8 +55,12 @@ test_expect_success "Should generage CamelCase folders for stubs" \
 test_expect_success "Should properly generate return type for Collections" \
   "test_cmp expected/Issue_2026.zep.php ide/0.0.1/Stubs/Issue_2026.zep.php"
 
-# See: https://github.com/phalcon/zephir/issues/2092
-test_expect_success "Should properly generate return type for type hinted object" \
-  "test_cmp expected/Issue_2092.zep.php ide/0.0.1/Stubs/Issue_2092.zep.php"
+if test_have_prereq PHP70; then
+  say 'skipping generation stubs tests with return types not supported in PHP 7.0.x'
+else
+  # See: https://github.com/phalcon/zephir/issues/2092
+  test_expect_success "Should properly generate return type for type hinted object" \
+    "test_cmp expected/Issue_2092.zep.php ide/0.0.1/Stubs/Issue_2092.zep.php"
+fi
 
 test_done
