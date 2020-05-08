b6cb985cad8ad6068b5ee1930880e2536db6e0ee
Serghei Iakovlev <egrep@protonmail.ch>
2020-05-08 13:10:00 +0300

    Correct ext-bootstrap.php

--- tests/ext-bootstrap.php
+++ tests/ext-bootstrap.php
@@ -15,11 +15,15 @@ if (!extension_loaded('phalcon')) {
     include_once __DIR__.'/../prototypes/phalcon.php';
 }
 
-if (!extension_loaded('test')) {
+if (!extension_loaded('stub')) {
     if ('1' == ini_get('enable_dl')) {
         $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
-        dl($prefix.'test.'.PHP_SHLIB_SUFFIX);
+        dl($prefix.'stub.'.PHP_SHLIB_SUFFIX);
     } else {
-        exit('"test" extension not loaded; cannot run tests without it');
+        $message = sprintf("The 'stub' extension not loaded; cannot run tests without it");
+        $line = str_repeat('-', strlen($message) + 4);
+
+        $message = sprintf("%s\n| %s |\n%s\n\n", $line, $message, $line);
+        exit($message);
     }
 }
