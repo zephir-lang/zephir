50b510f60c898f0ebbc7888bea52555dd4cee8ae
Anton Vasiliev <aionbase@gmail.com>
2021-01-25 17:20:52 +0000

    Refactor OoTest test class

--- tests/Extension/OoTest.php
+++ tests/Extension/OoTest.php
@@ -16,7 +16,7 @@ use Stub\Oo;
 
 class OoTest extends TestCase
 {
-    public function testAssertations()
+    public function testAssertations(): void
     {
         $test = new \Stub\Oo();
 
@@ -25,55 +25,55 @@ class OoTest extends TestCase
         $this->assertEquals($test->testInstance3(), new \Stub\Oo\OoNoConstruct());
 
         $obj4 = $test->testInstance4();
-        $this->assertInternalType('object', $obj4);
+        $this->assertIsObject($obj4);
         $this->assertSame($obj4->getA(), 'a');
         $this->assertSame($obj4->getB(), 'b');
 
         $obj5 = $test->testInstance5();
-        $this->assertInternalType('object', $obj5);
+        $this->assertIsObject($obj5);
         $this->assertSame($obj5->getA(), 'a');
         $this->assertSame($obj5->getB(), 'b');
 
         $obj6 = $test->testInstance6();
-        $this->assertInternalType('object', $obj6);
+        $this->assertIsObject($obj6);
         $this->assertSame($obj6->getA(), 1);
         $this->assertSame($obj6->getB(), 2);
 
         $obj7 = $test->testInstance7();
-        $this->assertInternalType('object', $obj7);
+        $this->assertIsObject($obj7);
         $this->assertSame($obj7->getA(), false);
         $this->assertSame($obj7->getB(), true);
 
         $obj8 = $test->testInstance8();
-        $this->assertInternalType('object', $obj8);
+        $this->assertIsObject($obj8);
         $this->assertSame($obj8->getA(), 1.2);
         $this->assertSame($obj8->getB(), 7.30);
 
         $obj9 = $test->testInstance9();
-        $this->assertInternalType('object', $obj9);
-        $this->assertInstanceOf('Stub\Oo\OoDynamicA', $obj9);
+        $this->assertIsObject($obj9);
+        $this->assertInstanceOf(\Stub\Oo\OoDynamicA::class, $obj9);
 
         $obj10 = $test->testInstance10();
-        $this->assertInternalType('object', $obj10);
-        $this->assertInstanceOf('Stub\Oo\OoDynamicB', $obj10);
+        $this->assertIsObject($obj10);
+        $this->assertInstanceOf(\Stub\Oo\OoDynamicB::class, $obj10);
 
         $obj = $test->testInstance11();
-        $this->assertInternalType('object', $obj);
-        $this->assertInstanceOf('Stub\Oo\OoConstructParams', $obj);
+        $this->assertIsObject($obj);
+        $this->assertInstanceOf(\Stub\Oo\OoConstructParams::class, $obj);
 
         $obj12 = $test->testInstance12();
-        $this->assertInternalType('object', $obj12);
+        $this->assertIsObject($obj12);
         $this->assertInstanceOf('Stub\Oo\OoDynamicA', $obj12);
     }
 
     /**
      * @issue https://github.com/phalcon/zephir/issues/1673
      */
-    public function testShouldDinamicalyCreateInstancesInLoop()
+    public function testShouldDinamicalyCreateInstancesInLoop(): void
     {
         $test = new Oo();
 
-        $this->assertEquals(
+        $this->assertSame(
             ['A', 'AA', 'B', 'BB'],
             $test->createInstancesInLoop()
         );
