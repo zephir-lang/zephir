d32e8a6ea36ca9bf0c88cd1e521378e14190fd90
dreamsxin <dreamsxin@126.com>
2021-02-02 11:16:10 +0800

    Updated php8

--- kernels/ZendEngine3/fcall.c
+++ kernels/ZendEngine3/fcall.c
@@ -165,15 +165,36 @@ static void resolve_callable(zval* retval, zephir_call_type type, zend_class_ent
 		zval q;
 		switch (type) {
 			case zephir_fcall_parent:
+#if PHP_VERSION_ID >= 80000
+			{
+				zend_class_entry* called_scope = zend_get_called_scope(EG(current_execute_data));
+				assert(called_scope && called_scope->parent);
+				zend_string_addref(called_scope->parent->name);
+				ZVAL_STR(&q, called_scope->parent->name);
+				ZEND_HASH_FILL_ADD(&q);
+			}
+
+#else
 				zend_string_addref(i_parent);
 				ZVAL_STR(&q, i_parent);
 				ZEND_HASH_FILL_ADD(&q);
+#endif
 				break;
 
 			case zephir_fcall_self:
+#if PHP_VERSION_ID >= 80000
+			{
+				zend_class_entry* called_scope = zend_get_called_scope(EG(current_execute_data));
+				assert(called_scope);
+				zend_string_addref(called_scope->name);
+				ZVAL_STR(&q, called_scope->name);
+				ZEND_HASH_FILL_ADD(&q);
+			}
+#else
 				zend_string_addref(i_self);
 				ZVAL_STR(&q, i_self);
 				ZEND_HASH_FILL_ADD(&q);
+#endif
 				break;
 
 			case zephir_fcall_static:
