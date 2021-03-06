0fb42b4b61386de9891b6b3d8d35ffd2dbee991d
Anton Vasiliev <aionbase@gmail.com>
2021-03-06 22:44:51 +0000

    #2111 - Update unified expected for sharness tests

--- tests/fixtures/protodir/connectionexception.h
+++ tests/fixtures/protodir/connectionexception.h
@@ -9,6 +9,10 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_protodir_connectionexception_getrequest, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 ZEPHIR_INIT_FUNCS(protodir_connectionexception_method_entry) {
+#if PHP_VERSION_ID >= 80000
 	PHP_ME(ProtoDir_ConnectionException, getRequest, arginfo_protodir_connectionexception_getrequest, ZEND_ACC_PUBLIC)
+#else
+	PHP_ME(ProtoDir_ConnectionException, getRequest, NULL, ZEND_ACC_PUBLIC)
+#endif
 	PHP_FE_END
 };
