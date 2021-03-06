5008626ea7cfc19ee5d0ed7c46728d01e58e2a77
Anton Vasiliev <aionbase@gmail.com>
2021-03-06 21:47:58 +0000

    #2111 - Update expected of connectionexception.h

--- tests/fixtures/protodir/connectionexception.h
+++ tests/fixtures/protodir/connectionexception.h
@@ -5,7 +5,10 @@ ZEPHIR_INIT_CLASS(ProtoDir_ConnectionException);
 
 PHP_METHOD(ProtoDir_ConnectionException, getRequest);
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_protodir_connectionexception_getrequest, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
 ZEPHIR_INIT_FUNCS(protodir_connectionexception_method_entry) {
-	PHP_ME(ProtoDir_ConnectionException, getRequest, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(ProtoDir_ConnectionException, getRequest, arginfo_protodir_connectionexception_getrequest, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
