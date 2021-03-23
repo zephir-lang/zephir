30f70e10cd7ceefe5f76c8ef6593f58b8679cfe2
Anton Vasiliev <aionbase@gmail.com>
2021-03-23 20:23:26 +0000

    #2150 - Fix t0001-compile.sh sharness test

--- tests/sharness/t0001-compile.sh
+++ tests/sharness/t0001-compile.sh
@@ -10,6 +10,7 @@ regexp="'-O2 -fvisibility=hidden -Wparentheses\( -flto\)\? -DZEPHIR_RELEASE=1'"
 test_expect_success "Compile the extension in production mode" "
   cd $FIXTURESDIR/devmode &&
   zephirc fullclean 2>&1 >/dev/null &&
+  zephirc generate --no-dev 2>&1 >/dev/null &&
   zephirc compile --no-dev 2>&1 >/dev/null &&
   cat ext/config.nice | grep -q -e \"^CFLAGS=$regexp\"
 "
@@ -18,6 +19,7 @@ regexp="'-O0 -g3'"
 test_expect_success "Compile the extension in development mode" "
   cd $FIXTURESDIR/devmode &&
   zephirc fullclean 2>&1 >/dev/null &&
+  zephirc generate --dev 2>&1 >/dev/null &&
   zephirc compile --dev 2>&1 >/dev/null &&
   cat ext/config.nice | grep -q -e \"^CFLAGS=$regexp\"
 "
