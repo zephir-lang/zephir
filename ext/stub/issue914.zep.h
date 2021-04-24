4a29beabe22bc1333c20c54143f9fe32468c838e
Anton Vasiliev <aionbase@gmail.com>
2021-04-24 19:34:29 +0100

    Regenerate ext/ directory

---
+++ ext/stub/issue914.zep.h
@@ -0,0 +1,18 @@
+
+extern zend_class_entry *stub_issue914_ce;
+
+ZEPHIR_INIT_CLASS(Stub_Issue914);
+
+PHP_METHOD(Stub_Issue914, __construct);
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_stub_issue914___construct, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEPHIR_INIT_FUNCS(stub_issue914_method_entry) {
+#if PHP_VERSION_ID >= 80000
+	PHP_ME(Stub_Issue914, __construct, arginfo_stub_issue914___construct, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
+#else
+	PHP_ME(Stub_Issue914, __construct, NULL, ZEND_ACC_PUBLIC|ZEND_ACC_CTOR)
+#endif
+	PHP_FE_END
+};
