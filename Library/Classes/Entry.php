955b0971007574e9455d7bd830ecbf77729292c0
Anton Vasiliev <aionbase@gmail.com>
2021-07-12 23:29:00 +0100

    #2213 - Move class entry detection into own class Entry

---
+++ Library/Classes/Entry.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This file is part of the Zephir.
+ *
+ * (c) Phalcon Team <team@zephir-lang.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Zephir\Classes;
+
+use Zephir\ClassDefinition;
+use Zephir\CompilationContext;
+use Zephir\Exception;
+
+/**
+ * Zend Class Entry detector
+ */
+class Entry
+{
+    /**
+     * Class name
+     *
+     * As it was passed: partially or fully.
+     *
+     * @var string
+     */
+    private string $classname;
+
+    /**
+     * Class namespace
+     *
+     * From where it is called.
+     *
+     * @var string
+     */
+    private string $classNamespace;
+
+    /**
+     * @var CompilationContext
+     */
+    private CompilationContext $compilationContext;
+
+    /**
+     * Entry constructor.
+     *
+     * @param string $className
+     * @param string $classNamespace
+     * @param CompilationContext $compilationContext
+     */
+    public function __construct(string $className, string $classNamespace, CompilationContext $compilationContext)
+    {
+        $this->classname = $className;
+        $this->classNamespace = $classNamespace;
+        $this->compilationContext = $compilationContext;
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    public function get(): string
+    {
+        $className = $this->compilationContext->getFullName($this->classname);
+        $classNamespace = explode('\\', $className);
+
+        /**
+         * External class, we don't know its ClassEntry in C world.
+         */
+        if (!$this->isInternalClass($classNamespace[0])) {
+            $className = str_replace('\\', '\\\\', $className);
+
+            return 'zend_lookup_class_ex(zend_string_init_fast(SL("\\\\'.$className.'")), NULL, 0)';
+        }
+
+        $className = end($classNamespace);
+        array_pop($classNamespace);
+        $namespace = join('\\', $classNamespace);
+
+        return (new ClassDefinition($namespace, $className))->getClassEntry();
+    }
+
+    /**
+     * Detect if start of namespace class
+     * belongs to project namespace.
+     *
+     * @param string $className
+     * @return bool
+     */
+    public function isInternalClass(string $className): bool
+    {
+        return preg_match('/^'.$className.'/', $this->classNamespace) === 1;
+    }
+}
