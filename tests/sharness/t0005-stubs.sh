57e08adcab930f81f3b6d0300e4eb7570e0e6360
Anton Vasiliev <aionbase@gmail.com>
2021-10-03 14:51:43 +0100

    #2273 - Remove tests for PHP7.0

--- tests/sharness/t0005-stubs.sh
+++ tests/sharness/t0005-stubs.sh
@@ -14,11 +14,6 @@ zephir_stubs() {
   zephirc stubs --no-ansi 2>&1 >/dev/null
 }
 
-# Some stubs test requires PHP >= 7.1
-if [ "$PHP_VERSION_ID" -lt "70100" ]; then
-  test_set_prereq PHP70
-fi
-
 # This test generates Stubs once and all other tests uses this build artifacts
 test_expect_success "Should generate Stubs" \
   "zephir_stubs && test -d ./ide/0.0.1/Stubs"
@@ -31,14 +26,9 @@ test_expect_success "Should properly generate type hint" \
 test_expect_success "Should properly namespace imports (use block)" \
   "test_cmp expected/Issue_1778.php ide/0.0.1/Stubs/Issue_1778.php"
 
-if test_have_prereq PHP70; then
-  expected="Issue_1900_PHP70.php"
-else
-  expected="Issue_1900.php"
-fi
 # See: https://github.com/zephir-lang/zephir/issues/1900
 test_expect_success "Should properly generate return types for stubs" \
-  "test_cmp expected/$expected ide/0.0.1/Stubs/Issue_1900.php"
+  "test_cmp expected/Issue_1900.php ide/0.0.1/Stubs/Issue_1900.php"
 
 # See: https://github.com/zephir-lang/zephir/issues/1907
 test_expect_success "Should properly generate Namespace for extends" \
@@ -48,14 +38,9 @@ test_expect_success "Should properly generate Namespace for extends" \
 test_expect_success "Should properly generate Namespace for extends (slash)" \
   "test_cmp expected/Issue_1907.php ide/0.0.1/Stubs/Issue_1907.php"
 
-if test_have_prereq PHP70; then
-  expected="Issue_1986_PHP70.php"
-else
-  expected="Issue_1986.php"
-fi
 # See: https://github.com/zephir-lang/zephir/issues/1986
 test_expect_success "Should properly generate Aliases for use statements" \
-  "test_cmp expected/$expected ide/0.0.1/Stubs/Issue_1986.php"
+  "test_cmp expected/Issue_1986.php ide/0.0.1/Stubs/Issue_1986.php"
 
 # See: https://github.com/zephir-lang/zephir/issues/1896
 test_expect_success "Should generate CamelCase folders for stubs" \
@@ -65,13 +50,8 @@ test_expect_success "Should generate CamelCase folders for stubs" \
 test_expect_success "Should properly generate return type for Collections" \
   "test_cmp expected/Issue_2026.php ide/0.0.1/Stubs/Issue_2026.php"
 
-if test_have_prereq PHP70; then
-  expected="Issue_2092_PHP70.php"
-else
-  expected="Issue_2092.php"
-fi
 # See: https://github.com/zephir-lang/zephir/issues/2092
 test_expect_success "Should properly generate return type for type hinted object" \
-  "test_cmp expected/$expected ide/0.0.1/Stubs/Issue_2092.php"
+  "test_cmp expected/Issue_2092.php ide/0.0.1/Stubs/Issue_2092.php"
 
 test_done
