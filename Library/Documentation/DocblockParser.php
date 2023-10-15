7436a5be4951d2d2ece5a1f5589c618c7d881fde
Anton Vasiliev <1695147@gmail.com>
2023-10-15 18:38:06 +0100

    #2407 - Remove unused property `$lines`

--- Library/Documentation/DocblockParser.php
+++ Library/Documentation/DocblockParser.php
@@ -22,7 +22,6 @@ class DocblockParser
     protected $currentChar;
     protected $currentCharIndex;
     protected $currentLine;
-    protected $lines = [];
 
     // Parsing helpers
     protected $ignoreSpaces;
