55b3d839699b4fe1cbca4bdee36fdbcf9c7644fe
Anton Vasiliev <1695147@gmail.com>
2023-11-26 11:14:59 +0000

    Remove "Should generate valid code with inheritance of prototype interfaces" test

--- tests/sharness/t0002-generate.sh
+++ tests/sharness/t0002-generate.sh
@@ -13,12 +13,4 @@ test_expect_success "Life Cycle Test" "
   test_cmp ext/lifecycle.c expected3.c
 "
 
-# See: https://github.com/zephir-lang/zephir/issues/1758
-test_expect_success "Should generate valid code with inheritance of prototype interfaces" "
-  cd $FIXTURESDIR/protodir &&
-  zephirc fullclean 2>&1 >/dev/null &&
-  zephirc generate 2>&1 >/dev/null &&
-  test_cmp ext/protodir/connectionexception.zep.h connectionexception.h
-"
-
 test_done
