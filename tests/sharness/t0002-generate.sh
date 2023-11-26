6a122431daf8117805fb92461171ae322b5f744d
Anton Vasiliev <1695147@gmail.com>
2023-11-26 11:09:38 +0000

    Remove "Should correctly specify ARGINFO" test

--- tests/sharness/t0002-generate.sh
+++ tests/sharness/t0002-generate.sh
@@ -6,16 +6,6 @@ test_description="Test generate commands"
 # shellcheck disable=SC1091
 source ./setup.sh
 
-test_expect_success "Should correctly specify ARGINFO" "
-  cd $FIXTURESDIR/typehints &&
-  zephirc fullclean 2>&1 >/dev/null &&
-  zephirc generate 2>&1 >/dev/null &&
-  test_cmp ext/typehints.c expected3.c &&
-  test_cmp ext/typehints/args.zep.h expected_args3.h &&
-  test_cmp ext/typehints/retval.zep.h expected_retval3.h &&
-  test_cmp ext/typehints/both.zep.h expected_both3.h
-"
-
 test_expect_success "Life Cycle Test" "
   cd $FIXTURESDIR/lifecycle &&
   zephirc fullclean 2>&1 >/dev/null &&
