63c84ce2493fa6fe3b94f003417a550fa8bbcfa6
Anton Vasiliev <1695147@gmail.com>
2024-02-07 22:55:57 +0000

    #2405 - Suppress callable deprecations

--- kernel/fcall.c
+++ kernel/fcall.c
@@ -371,7 +371,7 @@ int zephir_call_user_function(
 
 		char *is_callable_error = NULL;
 		zend_execute_data *frame = EG(current_execute_data);
-		if (obj_ce || !zend_is_callable_at_frame(&callable, fci.object, frame, 0, &fcic, &is_callable_error)) {
+		if (obj_ce || !zend_is_callable_at_frame(&callable, fci.object, frame, IS_CALLABLE_SUPPRESS_DEPRECATIONS, &fcic, &is_callable_error)) {
 			if (is_callable_error) {
 				zend_error(E_WARNING, "%s", is_callable_error);
 				efree(is_callable_error);
@@ -591,7 +591,7 @@ int zephir_call_user_func_array_noex(zval *return_value, zval *handler, zval *pa
 	}
 
 	zend_execute_data *frame = EG(current_execute_data);
-	if (!zend_is_callable_at_frame(handler, NULL, frame, 0, &fci_cache, &is_callable_error)) {
+	if (!zend_is_callable_at_frame(handler, NULL, frame, IS_CALLABLE_SUPPRESS_DEPRECATIONS, &fci_cache, &is_callable_error)) {
 		if (is_callable_error) {
 			zend_error(E_WARNING, "%s", is_callable_error);
 			efree(is_callable_error);
