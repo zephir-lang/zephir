0242ed2271f8616afa0390bc0a3b9f7208de209e
Anton Vasiliev <1695147@gmail.com>
2025-09-28 19:35:09 +0100

    #2462 - Fix namespace usage in internal test for issue 1956

--- tests/sharness/t0007-internall-mcall.sh
+++ tests/sharness/t0007-internall-mcall.sh
@@ -6,7 +6,7 @@ test_description="Test internal methods call"
 # shellcheck disable=SC1091
 source ./setup.sh
 
-code="echo get_class((new InternalTest\\Test)->issue1956());"
+code="echo get_class((new \InternalTest\Test)->issue1956());"
 test_expect_success "Return 'this' inside a method when 'internal-call-transformation' is enabled" "
   cd $FIXTURESDIR/internal &&
   zephirc fullclean 2>&1 >/dev/null &&
