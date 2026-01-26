a7669313a25b24a80c4131ba2ae27f362d7e3753
Anton Vasiliev <Jeckerson@users.noreply.github.com>
2026-01-26 12:50:55 +0000

    Add unit tests for IStringType class

---
+++ tests/Zephir/Types/IStringTypeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the Zephir.
+ *
+ * (c) Phalcon Team <team@zephir-lang.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Zephir\Test\Types;
+
+use PHPUnit\Framework\TestCase;
+use Zephir\Types\IStringType;
+use Zephir\Types\Types;
+
+final class IStringTypeTest extends TestCase
+{
+    public function testShouldGetTypeName(): void
+    {
+        $stringType = new IStringType();
+
+        $this->assertSame(Types::T_STRING, $stringType->getTypeName());
+    }
+
+    public function testShouldHaveMethodMap(): void
+    {
+        $stringType = new IStringType();
+
+        $this->assertIsArray($stringType->methodMap);
+        $this->assertNotEmpty($stringType->methodMap);
+    }
+}
