7671dbae9f43aa7f3bcf199929b37efe135f99c2
Anton Vasiliev <Jeckerson@users.noreply.github.com>
2026-05-15 12:54:22 +0100

    Update `ext/` directory

---
+++ ext/stub/21__closure.zep.c
@@ -0,0 +1,45 @@
+
+#ifdef HAVE_CONFIG_H
+#include "../ext_config.h"
+#endif
+
+#include <php.h>
+#include "../php_ext.h"
+#include "../ext.h"
+
+#include <Zend/zend_operators.h>
+#include <Zend/zend_exceptions.h>
+#include <Zend/zend_interfaces.h>
+
+#include "kernel/main.h"
+#include "kernel/fcall.h"
+#include "kernel/memory.h"
+#include "kernel/object.h"
+
+
+ZEPHIR_INIT_CLASS(stub_21__closure)
+{
+	ZEPHIR_REGISTER_CLASS(stub, 21__closure, stub, 21__closure, stub_21__closure_method_entry, ZEND_ACC_FINAL_CLASS);
+
+	return SUCCESS;
+}
+
+PHP_METHOD(stub_21__closure, __invoke)
+{
+	zephir_method_globals *ZEPHIR_METHOD_GLOBALS_PTR = NULL;
+	zend_long ZEPHIR_LAST_CALL_STATUS;
+	zval *val, val_sub;
+	zval *this_ptr = getThis();
+
+	ZVAL_UNDEF(&val_sub);
+	ZEND_PARSE_PARAMETERS_START(1, 1)
+		Z_PARAM_ZVAL(val)
+	ZEND_PARSE_PARAMETERS_END();
+	ZEPHIR_METHOD_GLOBALS_PTR = pecalloc(1, sizeof(zephir_method_globals), 0);
+	zephir_memory_grow_stack(ZEPHIR_METHOD_GLOBALS_PTR, __func__);
+	zephir_fetch_params(1, 1, 0, &val);
+	ZEPHIR_RETURN_CALL_METHOD(this_ptr, "issue2321doubled", NULL, 0, val);
+	zephir_check_call_status();
+	RETURN_MM();
+}
+
