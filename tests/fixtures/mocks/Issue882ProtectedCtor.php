5b9de8b4a27c22d384fbc33ecc47d1f7658a4f6d
Anton Vasiliev <Jeckerson@users.noreply.github.com>
2026-05-29 13:06:49 +0000

    #882 - Fix init class with non public visibility constructor

---
+++ tests/fixtures/mocks/Issue882ProtectedCtor.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Zephir.
+ *
+ * (c) Phalcon Team <team@zephir-lang.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+/**
+ * @see https://github.com/zephir-lang/zephir/issues/882
+ */
+class Issue882ProtectedCtor
+{
+    protected function __construct()
+    {
+    }
+}
